45d7183d832b0a63a7b692a895c9db7c4e55f31f
lucasward <lucasward@2e8e966d-6415-0410-94b9-dc4e65435d03>
2007-08-21 14:04:42 +0000

    Added SynchronousJobLauncher that was missing from last night's build.

--- execution/src/main/java/org/springframework/batch/execution/bootstrap/SynchronousJobLauncher.java
+++ execution/src/main/java/org/springframework/batch/execution/bootstrap/SynchronousJobLauncher.java
@@ -1,16 +1,39 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.batch.execution.bootstrap;
 
 import org.springframework.batch.core.configuration.NoSuchJobConfigurationException;
+import org.springframework.batch.core.runtime.JobIdentifier;
 import org.springframework.batch.repeat.ExitStatus;
 
+/**
+ * 
+ * @author Lucas Ward
+ */
+
 public interface SynchronousJobLauncher {
-
-	boolean isRunning();
-
-	ExitStatus run();
-
-	ExitStatus run(String jobName) throws NoSuchJobConfigurationException;
-
-	void stop();
-
+	
+	public ExitStatus run();
+	
+	public ExitStatus run(String jobName) throws NoSuchJobConfigurationException;
+	
+	public ExitStatus run(JobIdentifier jobIdentifier) throws NoSuchJobConfigurationException;
+	
+	public void stop();
+	
+	public boolean isRunning();
+	
 }
