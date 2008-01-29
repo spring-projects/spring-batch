1cf29b8241adc91f6f1017776121b85fa5f443cf
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-01-29 18:13:12 +0000

    Missing commit (subclipse?)

---
+++ spring-batch-core/src/main/java/org/springframework/batch/core/domain/Job.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.domain;
+
+import java.util.List;
+
+import org.springframework.batch.io.exception.BatchCriticalException;
+import org.springframework.batch.repeat.ExitStatus;
+
+/**
+ * Batch domain object representing a job. Job is an explicit abstraction
+ * representing the configuration of a job specified by a developer. It should
+ * be noted that restart policy is applied to the job as a whole and not to a
+ * step.
+ * 
+ * @author Dave Syer
+ *
+ */
+public interface Job {
+
+	String getName();
+
+	List getSteps();
+
+	int getStartLimit();
+
+	boolean isRestartable();
+
+	ExitStatus run(JobExecution execution) throws BatchCriticalException;
+
+}
