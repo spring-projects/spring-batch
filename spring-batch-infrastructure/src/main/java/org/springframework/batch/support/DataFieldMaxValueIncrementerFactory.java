93760bd298b0197df474abffca008b6cd56b6151
lucasward <lucasward@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-03-03 20:23:14 +0000

    BATCH-407:Added repository factory to samples and created a factory for incrementers for that logic to obtain them could be unit tested.  Also renamed SimpelJobRepositoryFactoryBean to just JobRepositoryFactoryBean

---
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/support/DataFieldMaxValueIncrementerFactory.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2006-2008 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.support;
+
+import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer;
+
+/**
+ * Factory for creating {@link DataFieldMaxValueIncrementer} implementations
+ * based upon a provided string.
+ * 
+ * @author Lucas Ward
+ *
+ */
+public interface DataFieldMaxValueIncrementerFactory {
+
+	/**
+	 * Return the {@link DataFieldMaxValueIncrementer} for the provided database type.
+	 * 
+	 * @param databaseType string represented database type
+	 * @param incrementerName incrementer name to create. In many cases this may be the
+	 *  sequence name
+	 * @return incrementer
+	 * @throws IllegalArgumentException if databaseType is invalid type, or incrementerName
+	 * is null.
+	 */
+	public DataFieldMaxValueIncrementer getIncrementer(String databaseType, String incrementerName);
+	
+	/**
+	 * Returns boolean indicated whether or not the provided string is supported by this
+	 * factory.
+	 * 
+	 */
+	public boolean isSupportedIncrementerType(String databaseType);
+}
