61d50fcd40588cc289ca1efacd29bc13ad557073
lucasward <lucasward@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-03-21 16:52:08 +0000

    BATCH-377: small change to remove unused tables from test schema

--- spring-batch-core/src/test/resources/org/springframework/batch/core/resource/init-foo-schema-hsqldb.sql
+++ spring-batch-core/src/test/resources/org/springframework/batch/core/resource/init-foo-schema-hsqldb.sql
@@ -11,11 +11,3 @@ INSERT INTO t_foos (id, name, value) VALUES (2, 'bar2', 2);
 INSERT INTO t_foos (id, name, value) VALUES (3, 'bar3', 3);
 INSERT INTO t_foos (id, name, value) VALUES (4, 'bar4', 4);
 INSERT INTO t_foos (id, name, value) VALUES (5, 'bar5', 5);
-
-CREATE TABLE T_WRITE_FOOS (
-	ID BIGINT NOT NULL,
-	NAME VARCHAR(45),
-	VALUE INTEGER
-);
-
-ALTER TABLE T_WRITE_FOOS ADD PRIMARY KEY (ID);
