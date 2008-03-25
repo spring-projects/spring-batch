f5fcf3a6b6b77ea1cd50bafb3f3b64fb1ed2295e
robokaso <robokaso@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-03-25 17:02:28 +0000

    fixed build - lesson learned: samples don't cleanup output files themselves, so they need to be cleaned before each run (otherwise the output file already exists even when the job did nothing).
    CompositeItemWriter wasn't delegating flush and clear calls, only write.

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/CompositeItemWriter.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/CompositeItemWriter.java
@@ -3,15 +3,16 @@ package org.springframework.batch.item.support;
 import java.util.Iterator;
 import java.util.List;
 
-import org.springframework.batch.item.AbstractItemWriter;
+import org.springframework.batch.item.ClearFailedException;
+import org.springframework.batch.item.FlushFailedException;
 import org.springframework.batch.item.ItemWriter;
 
 /**
- * Runs a collection of ItemProcessors in fixed-order sequence.
+ * Calls a collection of ItemWriters in fixed-order sequence.
  * 
  * @author Robert Kasanicky
  */
-public class CompositeItemWriter extends AbstractItemWriter {
+public class CompositeItemWriter implements ItemWriter {
 
 	private List delegates;
 
@@ -25,6 +26,18 @@ public class CompositeItemWriter extends AbstractItemWriter {
 	public void write(Object data) throws Exception {
 		for (Iterator iterator = delegates.listIterator(); iterator.hasNext();) {
 			((ItemWriter) iterator.next()).write(data);
+		}
+	}
+
+	public void clear() throws ClearFailedException {
+		for (Iterator iterator = delegates.listIterator(); iterator.hasNext();) {
+			((ItemWriter) iterator.next()).clear();
+		}
+	}
+
+	public void flush() throws FlushFailedException {
+		for (Iterator iterator = delegates.listIterator(); iterator.hasNext();) {
+			((ItemWriter) iterator.next()).flush();
 		}
 	}
 
