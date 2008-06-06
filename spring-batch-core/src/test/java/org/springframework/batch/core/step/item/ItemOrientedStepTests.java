c2c33fc00bff5abbc08ee978df2333fc45a46ae5
robokaso <robokaso@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-06-06 14:06:38 +0000

    OPEN - BATCH-654: Transactions when commit fails
    reverted the previous solution, it wouldn't really help

--- spring-batch-core/src/test/java/org/springframework/batch/core/step/item/ItemOrientedStepTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/step/item/ItemOrientedStepTests.java
@@ -654,9 +654,9 @@ public class ItemOrientedStepTests extends TestCase {
 		catch (RuntimeException ex) {
 			assertEquals(BatchStatus.UNKNOWN, stepExecution.getStatus());
 			String msg = stepExecution.getExitStatus().getExitDescription();
-			assertTrue(msg.contains("Fatal error detected during commit - the execution is in undefined state and restart will not be possible"));
+			assertTrue(msg.contains("Fatal error detected during commit"));
 			msg = ex.getMessage();
-			assertTrue(msg.contains("Fatal error detected during commit - the execution is in undefined state and restart will not be possible"));
+			assertTrue(msg.contains("Fatal error detected during commit"));
 			// The original rollback was caused by this one:
 			assertEquals("Bar", ex.getCause().getMessage());
 		}
