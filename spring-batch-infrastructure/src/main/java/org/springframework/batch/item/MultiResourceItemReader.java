86056722993f18ff8c886f7c5fd9e7a189a5d168
robokaso <robokaso@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-06-16 11:47:28 +0000

    IN PROGRESS - BATCH-664: smarter restart for MultiResourceItemReader
    smarter restart but ugly code

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/MultiResourceItemReader.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/MultiResourceItemReader.java
@@ -1,5 +1,9 @@
 package org.springframework.batch.item;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
@@ -17,16 +21,168 @@ import org.springframework.util.ClassUtils;
  * 
  * @author Robert Kasanicky
  */
-public class MultiResourceItemReader extends AbstractBufferedItemReaderItemStream implements InitializingBean {
+public class MultiResourceItemReader extends ExecutionContextUserSupport implements ItemReader, ItemStream,
+		InitializingBean {
+
+	private static final String RESOURCE_INDEX = "resourceIndex";
+	
+	private static final String ITEM_COUNT = "itemCount";
+	
+	private static final Object END_OF_RESOURCE_MARKER = new Object();
 
 	private ResourceAwareItemReaderItemStream delegate;
 
 	private Resource[] resources;
 
 	private int currentResourceIndex = 0;
+	
+	private int lastMarkedResourceIndex = 0;
+	
+	private long currentResourceItemCount = 0;
+	
+	private long lastMarkedResourceItemCount = 0;
+
+	private List itemBuffer = new ArrayList();
+
+	private ListIterator itemBufferIterator = null;
+
+	private boolean shouldReadBuffer = false;
+
+	private boolean saveState = false;
 
 	public MultiResourceItemReader() {
 		setName(ClassUtils.getShortName(MultiResourceItemReader.class));
+	}
+
+	/**
+	 * Reads the next item, jumping to next resource if necessary.
+	 */
+	public Object read() throws Exception, UnexpectedInputException, NoWorkFoundException, ParseException {
+
+		if (shouldReadBuffer) {
+			if (itemBufferIterator.hasNext()) {
+				Object buffered = itemBufferIterator.next();
+				while (buffered == END_OF_RESOURCE_MARKER) {
+					currentResourceIndex++;
+					buffered = itemBufferIterator.next();
+				}
+				currentResourceItemCount++;
+				return buffered;
+			}
+			else {
+				// buffer is exhausted, continue reading from file
+				shouldReadBuffer = false;
+				itemBufferIterator = null;
+			}
+		}
+
+		Object item = delegate.read();
+		currentResourceItemCount++;
+
+		while (item == null) {
+
+			if (++currentResourceIndex >= resources.length) {
+				currentResourceItemCount = 0;
+				return null;
+			}
+			delegate.close(new ExecutionContext());
+			delegate.setResource(resources[currentResourceIndex]);
+			delegate.open(new ExecutionContext());
+			item = delegate.read();
+			itemBuffer.add(END_OF_RESOURCE_MARKER);
+			currentResourceItemCount = 1;
+
+		}
+
+		itemBuffer.add(item);
+
+		return item;
+	}
+
+	/**
+	 * Clears the item buffer and cancels reading from buffer if it applies.
+	 * 
+	 * @see ItemReader#mark()
+	 */
+	public void mark() throws MarkFailedException {
+		if (!shouldReadBuffer) {
+			itemBuffer.clear();
+			itemBufferIterator = null;
+		}
+		else {
+			itemBuffer = itemBuffer.subList(itemBufferIterator.nextIndex(), itemBuffer.size());
+			itemBufferIterator = itemBuffer.listIterator();
+		}
+		
+		lastMarkedResourceIndex = currentResourceIndex;
+		lastMarkedResourceItemCount = currentResourceItemCount;
+		delegate.mark();
+	}
+
+	/**
+	 * Switches to 'read from buffer' state.
+	 * 
+	 * @see ItemReader#reset()
+	 */
+	public void reset() throws ResetFailedException {
+		shouldReadBuffer = true;
+		itemBufferIterator = itemBuffer.listIterator();
+		currentResourceIndex = lastMarkedResourceIndex;
+		currentResourceItemCount = lastMarkedResourceItemCount;
+	}
+
+	/**
+	 * Close the {@link #setDelegate(ResourceAwareItemReaderItemStream)} reader
+	 * and reset instance variable values.
+	 */
+	public void close(ExecutionContext executionContext) throws ItemStreamException {
+		shouldReadBuffer = false;
+		lastMarkedResourceIndex = 0;
+		currentResourceIndex = 0;
+		currentResourceItemCount = 0;
+		lastMarkedResourceItemCount = 0;
+		itemBufferIterator = null;
+		itemBuffer.clear();
+		delegate.close(executionContext);
+	}
+
+	/**
+	 * Figure out which resource to start with in case of restart and open the
+	 * delegate.
+	 */
+	public void open(ExecutionContext executionContext) throws ItemStreamException {
+
+		if (executionContext.containsKey(getKey(RESOURCE_INDEX))) {
+			currentResourceIndex = Long.valueOf(executionContext.getLong(getKey(RESOURCE_INDEX))).intValue();
+		}
+		
+		if (executionContext.containsKey(getKey(ITEM_COUNT))) {
+			currentResourceItemCount = executionContext.getLong(getKey(ITEM_COUNT));
+		}
+
+		delegate.setResource(resources[currentResourceIndex]);
+
+		delegate.open(new ExecutionContext());
+		
+		for (int i = 0; i < currentResourceItemCount; i++) {
+			try {
+				delegate.read();
+				delegate.mark();
+			}
+			catch (Exception e) {
+				throw new ItemStreamException("Could not restore position on restart", e);
+			}
+		}
+	}
+
+	/**
+	 * Store the current resource index and delegate's data.
+	 */
+	public void update(ExecutionContext executionContext) throws ItemStreamException {
+		if (saveState) {
+			executionContext.putLong(getKey(RESOURCE_INDEX), currentResourceIndex);
+			executionContext.putLong(getKey(ITEM_COUNT), currentResourceItemCount);
+		}
 	}
 
 	/**
@@ -36,12 +192,8 @@ public class MultiResourceItemReader extends AbstractBufferedItemReaderItemStrea
 		this.delegate = delegate;
 	}
 
-	/**
-	 * Make sure resources are set.
-	 */
 	public void afterPropertiesSet() throws Exception {
 		Assert.notEmpty(resources, "There must be at least one input resource");
-		Assert.notNull(delegate);
 	}
 
 	/**
@@ -52,47 +204,14 @@ public class MultiResourceItemReader extends AbstractBufferedItemReaderItemStrea
 	}
 
 	/**
-	 * Close the delegate and reset resource index.
+	 * Set the boolean indicating whether or not state should be saved in the
+	 * provided {@link ExecutionContext} during the {@link ItemStream} call to
+	 * update.
 	 * 
-	 * @see AbstractBufferedItemReaderItemStream#doClose()
+	 * @param saveState
 	 */
-	protected void doClose() throws Exception {
-		currentResourceIndex = 0;
-		delegate.close(new ExecutionContext());
-	}
-
-	/**
-	 * Open the delegate pointing it to the first resource.
-	 * 
-	 * @see AbstractBufferedItemReaderItemStream#doOpen()
-	 */
-	protected void doOpen() throws Exception {
-		delegate.setResource(resources[0]);
-		delegate.open(new ExecutionContext());
-	}
-
-	/**
-	 * Use the delegate to read next item, jump to next resource if the item is
-	 * null.
-	 * 
-	 * @see AbstractBufferedItemReaderItemStream#doRead()
-	 */
-	protected Object doRead() throws Exception {
-
-		Object item = delegate.read();
-
-		while (item == null) {
-
-			if (++currentResourceIndex >= resources.length) {
-				return null;
-			}
-			delegate.close(new ExecutionContext());
-			delegate.setResource(resources[currentResourceIndex]);
-			delegate.open(new ExecutionContext());
-			item = delegate.read();
-		}
-
-		return item;
+	public void setSaveState(boolean saveState) {
+		this.saveState = saveState;
 	}
 
 }
