7f1d7d2e95a85ecf6e84245437fb58caa4c0a075
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-06-24 14:48:05 +0000

    Prevent exec:exec being called during install

---
+++ archetypes/simple-cli/src/test/java/example/ExampleJobConfigurationTests.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example;
+
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Dave Syer
+ *
+ */
+public class ExampleJobConfigurationTests extends AbstractDependencyInjectionSpringContextTests {
+	
+	private JobLauncher jobLauncher;
+	
+	/**
+	 * Public setter for the {@link JobLauncher}.
+	 * @param jobLauncher the {@link JobLauncher} to set
+	 */
+	public void setJobLauncher(JobLauncher jobLauncher) {
+		this.jobLauncher = jobLauncher;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.test.AbstractSingleSpringContextTests#getConfigLocations()
+	 */
+	protected String[] getConfigLocations() {
+		return new String[] {"example/simpleJob.xml"};
+	}
+	
+	public void testSimpleProperties() throws Exception {
+		assertNotNull(jobLauncher);
+	}
+}
