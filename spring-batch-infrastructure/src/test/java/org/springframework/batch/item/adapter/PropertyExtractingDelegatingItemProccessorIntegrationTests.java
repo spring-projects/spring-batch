b3eec37c7932df38c2a6861d6db5c4ebab863b92
trisberg <trisberg@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-07-30 17:09:51 +0000

    IN PROGRESS - BATCH-710: updated tests to use SpringJUnit4ClassRunner and SimpleJdbcTemplate

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/adapter/PropertyExtractingDelegatingItemProccessorIntegrationTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/adapter/PropertyExtractingDelegatingItemProccessorIntegrationTests.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import org.springframework.batch.item.sample.Foo;
 import org.springframework.batch.item.sample.FooService;
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,8 +43,8 @@ public class PropertyExtractingDelegatingItemProccessorIntegrationTests {
 		assertFalse(fooService.getProcessedFooNameValuePairs().isEmpty());
 
 		for (int i = 0; i < input.size(); i++) {
-			Foo inputFoo = (Foo) input.get(i);
-			Foo outputFoo = (Foo) processed.get(i);
+			Foo inputFoo = input.get(i);
+			Foo outputFoo = processed.get(i);
 			assertEquals(inputFoo.getName(), outputFoo.getName());
 			assertEquals(inputFoo.getValue(), outputFoo.getValue());
 			assertEquals(0, outputFoo.getId());
