db78b5a1b9b6920e2d83d19aaf01295f463def16
robokaso <robokaso@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-07-30 14:28:30 +0000

    fix the build - moved misplaced tests

--- spring-batch-core/src/test/java/org/springframework/batch/core/repository/dao/MapExecutionContextDaoTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/repository/dao/MapExecutionContextDaoTests.java
@@ -1,8 +1,12 @@
 package org.springframework.batch.core.repository.dao;
 
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
 /**
  * Tests for {@link MapExecutionContextDao}.
  */
+@RunWith(JUnit4ClassRunner.class)
 public class MapExecutionContextDaoTests extends AbstractExecutionContextDaoTests {
 
 	@Override
