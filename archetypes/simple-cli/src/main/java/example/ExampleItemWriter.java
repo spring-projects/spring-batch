a49d0531dab07e79aea052974563a91c818a4341
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-08-20 10:00:21 +0000

    RESOLVED - issue BATCH-788: Remove flush/clear from ItemWriter
    Done

--- archetypes/simple-cli/src/main/java/example/ExampleItemWriter.java
+++ archetypes/simple-cli/src/main/java/example/ExampleItemWriter.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.AbstractItemWriter;
 
 
 /**
  * Dummy {@link ItemWriter} which only logs data it receives.
  */
-public class ExampleItemWriter extends AbstractItemWriter<Object> {
+public class ExampleItemWriter implements ItemWriter<Object> {
 
 	private static final Log log = LogFactory.getLog(ExampleItemWriter.class);
 	
