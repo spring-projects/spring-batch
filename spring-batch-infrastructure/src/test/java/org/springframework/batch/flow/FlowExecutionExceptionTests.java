c862c6f0655e7e13b0fa14d60da60e682c942f65
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-10-27 14:29:35 +0000

    OPEN - issue BATCH-679: Non-sequential execution
    Add test for exception

---
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/flow/FlowExecutionExceptionTests.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.flow;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * @author Dave Syer
+ *
+ */
+public class FlowExecutionExceptionTests {
+
+	/**
+	 * Test method for {@link FlowExecutionException#FlowExecutionException(String)}.
+	 */
+	@Test
+	public void testFlowExecutionExceptionString() {
+		FlowExecutionException exception = new FlowExecutionException("foo");
+		assertEquals("foo", exception.getMessage());
+	}
+
+	/**
+	 * Test method for {@link FlowExecutionException#FlowExecutionException(String, Throwable)}.
+	 */
+	@Test
+	public void testFlowExecutionExceptionStringThrowable() {
+		FlowExecutionException exception = new FlowExecutionException("foo", new RuntimeException("bar"));
+		assertEquals("foo", exception.getMessage());
+		assertEquals("bar", exception.getCause().getMessage());
+	}
+
+}
