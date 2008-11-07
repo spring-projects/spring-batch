ec2a60715b288b3e025ccf881c87c5243419e84b
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-11-07 18:58:17 +0000

    fix duplicate job launch

--- spring-batch-core/src/test/java/org/springframework/batch/core/annotation/StepComponentBeanPostProcessorIntegrationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/annotation/StepComponentBeanPostProcessorIntegrationTests.java
@@ -1,9 +1,11 @@
 package org.springframework.batch.core.annotation;
 
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.job.SimpleJob;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,7 @@ public class StepComponentBeanPostProcessorIntegrationTests {
 	@Test
 	public void testListener() throws Exception{
 		
-		jobLauncher.run(job, new JobParameters());
+		jobLauncher.run(job, new JobParametersBuilder().addDate("run.date", new Date()).toJobParameters());
 		
 	//	assertTrue(TestComponent.isAfterStepCalled());
 	//	assertTrue(TestComponent.isBeforeStepCalled());
