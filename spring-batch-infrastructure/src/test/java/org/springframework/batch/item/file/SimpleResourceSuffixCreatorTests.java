8ef9735fb449cb213ceb47fd809ba8efbcdbdfb8
robokaso <robokaso@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-11-10 11:52:55 +0000

    IN PROGRESS - BATCH-34: Support for multiple I/O files in a single jobRun for a particular scheduleDate.
    fixed failing test

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/SimpleResourceSuffixCreatorTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/SimpleResourceSuffixCreatorTests.java
@@ -13,8 +13,8 @@ public class SimpleResourceSuffixCreatorTests {
 
 	@Test
 	public void testGetSuffix() {
-		assertEquals("0", tested.getSuffix(0));
-		assertEquals("1", tested.getSuffix(1));
-		assertEquals("3463457", tested.getSuffix(3463457));
+		assertEquals(".0", tested.getSuffix(0));
+		assertEquals(".1", tested.getSuffix(1));
+		assertEquals(".3463457", tested.getSuffix(3463457));
 	}
 }
