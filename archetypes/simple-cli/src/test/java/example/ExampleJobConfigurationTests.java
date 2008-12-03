be39978171ac5ee2f54d5946b28c66c7ffee2e81
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-12-03 20:11:22 +0000

    Make archetype/template standalone project (depends on core not samples)

--- archetypes/simple-cli/src/test/java/example/ExampleJobConfigurationTests.java
+++ archetypes/simple-cli/src/test/java/example/ExampleJobConfigurationTests.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -34,9 +36,18 @@ public class ExampleJobConfigurationTests {
 	
 	@Autowired
 	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job job;
 	
 	@Test
 	public void testSimpleProperties() throws Exception {
 		assertNotNull(jobLauncher);
 	}
+	
+	@Test
+	public void testLaunchJob() throws Exception {
+		jobLauncher.run(job, new JobParameters());
+	}
+	
 }
