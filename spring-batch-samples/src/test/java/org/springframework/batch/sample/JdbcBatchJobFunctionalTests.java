e8ef2ae31d0a17133e554702c5abb8df6d61df02
trisberg <trisberg@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-12-11 17:47:35 +0000

    BATCH-965: renamed batch writer to JdbcBatchItemWriter and added named parameter support, added JPA and Hibernate writers

--- spring-batch-samples/src/test/java/org/springframework/batch/sample/JdbcBatchJobFunctionalTests.java
+++ spring-batch-samples/src/test/java/org/springframework/batch/sample/JdbcBatchJobFunctionalTests.java
@@ -5,12 +5,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * Test for BatchSqlUpdateJob - checks that customer credit has been updated to expected value.
+ * Test for JdbcBatchItemWriterJob - checks that customer credit has been updated to expected value.
  * 
  * @author Robert Kasanicky
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class BatchSqlUpdateJobFunctionalTests extends AbstractCustomerCreditIncreaseTests {
+public class JdbcBatchJobFunctionalTests extends AbstractCustomerCreditIncreaseTests {
 
 }
