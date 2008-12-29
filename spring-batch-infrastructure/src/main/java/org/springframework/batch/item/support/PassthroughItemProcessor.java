aab745e5915e687e606425c6be12b1cebea6daf8
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2008-12-29 17:28:37 +0000

    Javadocs for PassthroughItemProcessor

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/PassthroughItemProcessor.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/PassthroughItemProcessor.java
@@ -3,11 +3,24 @@ package org.springframework.batch.item.support;
 import org.springframework.batch.item.ItemProcessor;
 
 /**
+ * Simple {@link ItemProcessor} that does nothing - simply passes its argument
+ * through to the caller. Useful as a default when the reader and writer in a
+ * business process deal with items of the same type, and no transformations are
+ * required.
+ * 
  * @author Dave Syer
- *
+ * 
  */
 public class PassthroughItemProcessor<T> implements ItemProcessor<T, T> {
+
+	/**
+	 * Just returns the item back to the caller.
+	 * 
+	 * @return the item
+	 * @see ItemProcessor#process(Object)
+	 */
 	public T process(T item) throws Exception {
 		return item;
 	}
+
 }
