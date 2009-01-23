baff82b05735449633fe623b9bfeb79d8dbeb5f9
trisberg <trisberg@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-01-23 15:49:12 +0000

    BATCH-1001: applied changes from the patch to set restartable to true as default

--- spring-batch-core/src/main/java/org/springframework/batch/core/job/AbstractJob.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/AbstractJob.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 the original author or authors.
+ * Copyright 2006-2009 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ public abstract class AbstractJob implements Job, BeanNameAware, InitializingBea
 
 	private String name;
 
-	private boolean restartable = false;
+	private boolean restartable = true;
 
 	private JobRepository jobRepository;
 
