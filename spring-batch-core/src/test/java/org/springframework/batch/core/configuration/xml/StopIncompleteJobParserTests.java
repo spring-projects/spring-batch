d306c418fa8910e89effda26c5058577cdfdab75
dhgarrette <dhgarrette@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-02-07 15:56:47 +0000

    BATCH-1011: Added new test case to show 'pause' element's ability to skip a FAILED step on restart.

---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/StopIncompleteJobParserTests.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.configuration.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StopIncompleteJobParserTests extends AbstractJobParserTests {
+
+	@Test
+	public void testStopIncomplete() throws Exception {
+
+		//
+		// First Launch
+		//
+		JobExecution jobExecution = createJobExecution();
+		job.execute(jobExecution);
+		assertEquals(1, stepNamesList.size());
+		assertTrue(stepNamesList.contains("failingStep"));
+
+		assertEquals(BatchStatus.STOPPED, jobExecution.getStatus());
+		assertEquals(ExitStatus.FAILED.getExitCode(), jobExecution.getExitStatus().getExitCode());
+
+		StepExecution stepExecution1 = getStepExecution(jobExecution, "failingStep");
+		assertEquals(BatchStatus.INCOMPLETE, stepExecution1.getStatus());
+		assertEquals(ExitStatus.FAILED.getExitCode(), stepExecution1.getExitStatus().getExitCode());
+
+		//
+		// Second Launch
+		//
+		stepNamesList.clear();
+		jobExecution = createJobExecution();
+		job.execute(jobExecution);
+		assertEquals(1, stepNamesList.size()); // step1 is not executed
+		assertTrue(stepNamesList.contains("step2"));
+
+		assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+		assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+
+		StepExecution stepExecution2 = getStepExecution(jobExecution, "step2");
+		assertEquals(BatchStatus.COMPLETED, stepExecution2.getStatus());
+		assertEquals(ExitStatus.COMPLETED, stepExecution2.getExitStatus());
+
+	}
+
+	public static class TestDecider implements JobExecutionDecider {
+		public String decide(JobExecution jobExecution, StepExecution stepExecution) {
+			return "FOO";
+		}
+	}
+
+}
