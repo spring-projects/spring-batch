bc679464fc017e0a8e5c6aad56b55f0c4d377c19
dhgarrette <dhgarrette@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-02-16 23:26:33 +0000

    BATCH-1076: added test case for restart back on a step that failed.

---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/StopRestartOnFailedStepJobParserTests.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.configuration.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StopRestartOnFailedStepJobParserTests extends AbstractJobParserTests {
+
+	@Test
+	public void testStopRestartOnCompletedStep() throws Exception {
+
+		//
+		// First Launch
+		//
+		launchAndAssert();
+
+		//
+		// Second Launch
+		//
+		stepNamesList.clear();
+		launchAndAssert();
+
+	}
+
+	private void launchAndAssert() throws JobInstanceAlreadyCompleteException, JobRestartException,
+			JobExecutionAlreadyRunningException {
+		JobExecution jobExecution = createJobExecution();
+		job.execute(jobExecution);
+		assertEquals(1, stepNamesList.size());
+		assertTrue(stepNamesList.contains("fail"));
+
+		assertEquals(BatchStatus.STOPPED, jobExecution.getStatus());
+		assertEquals(ExitStatus.STOPPED.getExitCode(), jobExecution.getExitStatus().getExitCode());
+
+		StepExecution stepExecution1 = getStepExecution(jobExecution, "fail");
+		assertEquals(BatchStatus.FAILED, stepExecution1.getStatus());
+		assertEquals(ExitStatus.FAILED.getExitCode(), stepExecution1.getExitStatus().getExitCode());
+	}
+
+}
