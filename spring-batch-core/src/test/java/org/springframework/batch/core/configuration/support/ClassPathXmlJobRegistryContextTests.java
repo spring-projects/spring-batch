57cf2b1c0b4ba454177d1960b4f080ec22223fea
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-02-18 08:28:06 +0000

    BATCH-1087: add test case

---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/support/ClassPathXmlJobRegistryContextTests.java
@@ -0,0 +1,40 @@
+package org.springframework.batch.core.configuration.support;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 
+ * 
+ * @author Dave Syer
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class ClassPathXmlJobRegistryContextTests {
+
+	@Autowired
+	private ClassPathXmlJobRegistry registry;
+	
+	@Test
+	public void testLocateJob() throws Exception{
+		
+		Collection<String> names = registry.getJobNames();
+		assertEquals(1, names.size());
+		assertTrue(names.contains("test-job"));
+		
+		Job job = registry.getJob("test-job");
+		assertEquals("test-job", job.getName());
+
+	}
+	
+}
