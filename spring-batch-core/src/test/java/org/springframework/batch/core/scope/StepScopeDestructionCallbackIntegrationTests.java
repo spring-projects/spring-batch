84ba0a6153ee53934836264182398c1d36cae9cf
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-03-07 14:28:22 +0000

    Fixed assertion that broke build

--- spring-batch-core/src/test/java/org/springframework/batch/core/scope/StepScopeDestructionCallbackIntegrationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/scope/StepScopeDestructionCallbackIntegrationTests.java
@@ -56,7 +56,7 @@ public class StepScopeDestructionCallbackIntegrationTests {
 	public void testProxiedScopedProxy() throws Exception {
 		assertNotNull(nested);
 		nested.execute(new StepExecution("step", new JobExecution(0L), 1L));
-		assertEquals(2, TestAdvice.names.size());
+		assertEquals(4, TestAdvice.names.size());
 		assertEquals("bar", TestAdvice.names.get(0));
 		assertEquals("destroyed", TestDisposableCollaborator.message);
 	}
