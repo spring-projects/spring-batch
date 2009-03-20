de21d44af1221b4d026afd98f9f4838eda34dfc5
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-03-20 10:49:57 +0000

    Try to fix timout error in build again

--- spring-batch-infrastructure-tests/src/test/java/org/springframework/batch/repeat/jms/AsynchronousTests.java
+++ spring-batch-infrastructure-tests/src/test/java/org/springframework/batch/repeat/jms/AsynchronousTests.java
@@ -161,7 +161,7 @@ public class AsynchronousTests {
 		// Need to sleep here, but not too long or the
 		// container goes into its own recovery cycle and spits out the bad
 		// message...
-		waitFor(list,2,2000);
+		waitFor(list,2,500);
 
 		container.stop();
 
@@ -189,9 +189,9 @@ public class AsynchronousTests {
 	 */
 	private void waitFor(List<String> list, int size, int timeout) throws InterruptedException {
 		int count = 0;
-		int max = timeout / 100;
+		int max = timeout / 50;
 		while (count<max && list.size()<size) {
-			Thread.sleep(100);
+			Thread.sleep(50);
 		}
 	}
 
