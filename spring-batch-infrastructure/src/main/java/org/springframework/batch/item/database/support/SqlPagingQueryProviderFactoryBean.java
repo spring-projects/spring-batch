0bd6a8fc17e0b234c8fe0819dbe51b37c911d9d6
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-05-11 00:07:59 +0000

    Add cause exception when database type cannot be determined

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/SqlPagingQueryProviderFactoryBean.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/SqlPagingQueryProviderFactoryBean.java
@@ -139,7 +139,7 @@ public class SqlPagingQueryProviderFactoryBean implements FactoryBean {
 		}
 		catch (MetaDataAccessException e) {
 			throw new IllegalArgumentException(
-					"Could not inspect meta data for database type.  You have to supply it explicitly.");
+					"Could not inspect meta data for database type.  You have to supply it explicitly.", e);
 		}
 
 		AbstractSqlPagingQueryProvider provider = providers.get(type);
