3a25f23218da28b2a57c2433356e86c71b536a2e
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-06-16 07:49:49 +0000

    BATCH-1209: add a Groovy job in samples using AntBuilder to do some file schlepping

---
+++ spring-batch-samples/src/test/java/org/springframework/batch/sample/GroovyJobFunctionalTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.batch.sample;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration()
+public class GroovyJobFunctionalTests extends AbstractValidatingBatchLauncherTests {
+	
+	@Before
+	public void removeOldData() throws IOException {
+		FileUtils.deleteDirectory(new File("target/groovyJob"));
+	}
+
+	@Test
+	public void testLaunchJob() throws Exception{
+		super.testLaunchJob();
+	}
+
+	protected void validatePostConditions() {
+		assertTrue(new File("target/groovyJob/output/files.zip").exists());		
+	}
+
+	protected void validatePreConditions() {
+		assertFalse(new File("target/groovyJob/output/files.zip").exists());		
+	}
+	
+}
