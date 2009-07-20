9c9c0e42d93e737126b45f3349619ae813ec7083
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-07-20 07:25:06 +0000

    Add integration test for stop on custom exit status

--- spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/TestCustomStatusListener.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/TestCustomStatusListener.java
@@ -3,21 +3,15 @@ package org.springframework.batch.core.configuration.xml;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
-import org.springframework.batch.core.annotation.AfterRead;
 
-public class TestListener extends AbstractTestComponent implements StepExecutionListener {
+public class TestCustomStatusListener extends AbstractTestComponent implements StepExecutionListener {
 
 	public ExitStatus afterStep(StepExecution stepExecution) {
-		return null;
+		return new ExitStatus("FOO").and(stepExecution.getExitStatus());
 	}
 
 	public void beforeStep(StepExecution stepExecution) {
 		executed = true;
 	}
 	
-	@AfterRead
-	public void logItem(){
-		
-	}
-
 }
