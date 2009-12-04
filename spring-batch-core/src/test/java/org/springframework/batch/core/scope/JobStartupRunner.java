f37bdaa9d84895df4e56c7cc0edb1d80a4a2fd7e
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-12-04 19:14:58 +0000

    Don't use @PostConstruct

--- spring-batch-core/src/test/java/org/springframework/batch/core/scope/JobStartupRunner.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/scope/JobStartupRunner.java
@@ -1,12 +1,11 @@
 package org.springframework.batch.core.scope;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
+import org.springframework.beans.factory.InitializingBean;
 
-public class JobStartupRunner {
+public class JobStartupRunner implements InitializingBean {
 
 	private Step step;
 	
@@ -14,8 +13,7 @@ public class JobStartupRunner {
 		this.step = step;
 	}
 	
-	@PostConstruct
-	public void launch() throws Exception {
+	public void afterPropertiesSet() throws Exception {
 		StepExecution stepExecution = new StepExecution("step", new JobExecution(1L), 0L);
 		step.execute(stepExecution);
 		// expect no errors
