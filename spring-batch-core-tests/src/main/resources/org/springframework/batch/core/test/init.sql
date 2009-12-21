644efc30a06988f480a59bbe0a3cfa27aafee059
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-12-21 14:23:44 +0000

    OPEN - issue BATCH-1407: Integration tests for core (including multi-threaded long running tests)

--- spring-batch-core-tests/src/main/resources/org/springframework/batch/core/test/init.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE T_FOOS;
-create table T_FOOS (
-	id integer not null primary key,
-	name varchar(80),
-	foo_date timestamp
-);
--- spring-batch-core-tests/src/test/java/org/springframework/batch/core/test/repository/JobSupport.java
+++ spring-batch-core-tests/src/test/java/org/springframework/batch/core/test/repository/JobSupport.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.springframework.batch.sample.support;
+package org.springframework.batch.core.test.repository;
 
 import java.util.ArrayList;
 import java.util.List;
