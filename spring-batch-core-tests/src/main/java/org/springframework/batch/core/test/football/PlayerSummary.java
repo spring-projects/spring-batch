dc9fe073725af2f62036d9f1f59a1d98c9f94342
dsyer <dsyer@2e8e966d-6415-0410-94b9-dc4e65435d03>
2009-12-30 11:47:41 +0000

    BATCH-1470: remove commons-lang dependency

--- spring-batch-core-tests/src/main/java/org/springframework/batch/core/test/football/PlayerSummary.java
+++ spring-batch-core-tests/src/main/java/org/springframework/batch/core/test/football/PlayerSummary.java
@@ -16,8 +16,6 @@
 
 package org.springframework.batch.core.test.football;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 
 /**
  * Domain object representing the summary of a given Player's 
@@ -120,12 +118,29 @@ public class PlayerSummary {
 				";" + passingTd + ";" + interceptions + ";" + rushes + ";" + rushYards + ";" + receptions +
 				";" + receptionYards + ";" + totalTd;
 	}
-	
-	public boolean equals(Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
-	}
+	@Override
 	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlayerSummary other = (PlayerSummary) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		}
+		else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+		
 }
