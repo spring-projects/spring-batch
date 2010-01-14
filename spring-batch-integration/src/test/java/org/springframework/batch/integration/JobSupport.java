f0c147a18ce1921e9e44ab0f7330d96a7e252eef
David Syer <dsyer@vmware.com>
2010-01-14 22:15:41 +0000

    Stupid error with imports

--- spring-batch-integration/src/test/java/org/springframework/batch/integration/JobSupport.java
+++ spring-batch-integration/src/test/java/org/springframework/batch/integration/JobSupport.java
@@ -3,8 +3,8 @@ package org.springframework.batch.integration;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersIncrementer;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.job.JobParametersValidator;
 
 public class JobSupport implements Job {
 	
