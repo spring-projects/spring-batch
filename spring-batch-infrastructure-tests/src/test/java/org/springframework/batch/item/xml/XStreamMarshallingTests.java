0a77a5b4eddf11fbb5ab75ff70e8867788e4e612
Josh Long <josh.long@springsource.com>
2011-03-21 06:12:37 -0700

    BATCH-1532: fix integration tests to work with Spring 3

--- spring-batch-infrastructure-tests/src/test/java/org/springframework/batch/item/xml/XStreamMarshallingTests.java
+++ spring-batch-infrastructure-tests/src/test/java/org/springframework/batch/item/xml/XStreamMarshallingTests.java
@@ -4,12 +4,15 @@ import org.springframework.batch.item.xml.domain.Trade;
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.xstream.XStreamMarshaller;
 
+import java.util.Collections;
+
 public class XStreamMarshallingTests extends
 		AbstractStaxEventWriterItemWriterTests {
 
 	protected Marshaller getMarshaller() throws Exception {
 		XStreamMarshaller marshaller = new XStreamMarshaller();
-		marshaller.addAlias("trade", Trade.class);
+//		marshaller.addAlias("trade", Trade.class);
+		marshaller.setAliases(Collections.singletonMap("trade", Trade.class));
 		//in XStreamMarshaller.marshalSaxHandlers() method is used SaxWriter, which is configured
 		//to include enclosing document (SaxWriter.includeEnclosingDocument is always set to TRUE) 
 		return marshaller;
