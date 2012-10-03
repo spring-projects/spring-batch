64ca2608ad391b2b3ab4903b7b50e62026ca1db6
Michael Minella <michael@michaelminella.com>
2012-10-03 18:14:22 -0500

    BATCH-1684 - Allow serializer to be injected into JobRepository
    * Updated the job-repository tag to allow for a serializer and deserializer to be injected
    * Updated the XStreamExecutionContextStringSerializer to implement Serializer and Deserializer
    * Replaced the ExecutionContextStringSerializer with the ExecutionContextSerializer interface
    * Updated the XSD and parser to accept the injection of an implementation of the ExecutionContextSerializer from the job-repository tag
    * Updated the JdbcExecutionContextDao to use injected ExecutionContextSerializer implementation
    
    Reference: https://jira.springsource.org/browse/BATCH-1684

---
+++ spring-batch-core/src/main/java/org/springframework/batch/core/repository/ExecutionContextSerializer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2006-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.repository;
+
+import org.springframework.core.serializer.Deserializer;
+import org.springframework.core.serializer.Serializer;
+
+/**
+ * A composite interface that combines both serialization and deserialization
+ * of an execution context into a single implementation.  Implementations of this
+ * interface are used to serialize the execution context for persistence during
+ * the execution of a job.
+ *
+ * @author Michael Minella
+ * @since 2.2
+ * @see Serializer
+ * @see Deserializer
+ */
+public interface ExecutionContextSerializer extends Serializer, Deserializer {
+
+}
