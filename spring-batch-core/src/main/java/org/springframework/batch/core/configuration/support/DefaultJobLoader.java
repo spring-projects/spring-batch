20cc3b2f10a29fcb3b540465ebe4aa2d3cec4441
Stéphane Nicoll <stephane@nicoll.net>
2012-11-24 10:43:35 +0100

    Added the concept of a StepRegistry that tracks the Step instance attached to a Job.
    - Added a simple MapStepRegistry that keeps track of the step in a simple map per job
    - MapJobRegistry implements StepRegistry as well using the delegate pattern
    - DefaultJobLoader makes sure to initialize both registries now. Auto-detects that the JobRegistry implements StepRegistry
    - JobSupport now implements StepLocator so that such job can be loaded by the DefaultJobLoader
    - Also registering any "detached" step available for a particular job to that job if that wasn't done yet. Since a partition handler may link to any step by name, the step registry must be filled with any step that are available in the context of a job. If each job is located in its own application context, this is just fine. If more than one job is defined in the same context, the step registry may be able to return a step that is not related to that job but it shouldn't hurt as this happens only if a component is explicitly asking to execute a step by name for a particular job.

--- spring-batch-core/src/main/java/org/springframework/batch/core/configuration/support/DefaultJobLoader.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/configuration/support/DefaultJobLoader.java
@@ -24,24 +24,31 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.DuplicateJobException;
 import org.springframework.batch.core.configuration.JobFactory;
 import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.StepRegistry;
 import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.step.StepLocator;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.Assert;
 
 /**
  * Default implementation of {@link JobLoader}. Uses a {@link JobRegistry} to
  * manage a population of loaded jobs and clears them up when asked.
- * 
+ *
  * @author Dave Syer
- * 
+ *
  */
-public class DefaultJobLoader implements JobLoader {
+public class DefaultJobLoader implements JobLoader, InitializingBean {
 
 	private static Log logger = LogFactory.getLog(DefaultJobLoader.class);
 
 	private JobRegistry jobRegistry;
+    private StepRegistry stepRegistry;
 
 	private Map<ApplicationContextFactory, ConfigurableApplicationContext> contexts = new ConcurrentHashMap<ApplicationContextFactory, ConfigurableApplicationContext>();
 
@@ -51,30 +58,57 @@ public class DefaultJobLoader implements JobLoader {
 	 * Default constructor useful for declarative configuration.
 	 */
 	public DefaultJobLoader() {
-		this(null);
+		this(null, null);
 	}
 
+    /**
+     * Creates a job loader with the job registry provided.
+     * <p/>
+     * If the specified {@link JobRegistry} is also a {@link StepRegistry} it
+     * is registered as the step registry to use for this instance.
+     *
+     * @param jobRegistry a {@link JobRegistry}
+     */
+    public DefaultJobLoader(JobRegistry jobRegistry) {
+        this(jobRegistry, jobRegistry instanceof StepRegistry ? (StepRegistry) jobRegistry : null);
+    }
+
 	/**
-	 * Create a job loader with the job registry provided.
+	 * Creates a job loader with the job and step registries provided.
+     *
 	 * @param jobRegistry a {@link JobRegistry}
+     * @param stepRegistry a {@link StepRegistry}
 	 */
-	public DefaultJobLoader(JobRegistry jobRegistry) {
+	public DefaultJobLoader(JobRegistry jobRegistry, StepRegistry stepRegistry) {
 		this.jobRegistry = jobRegistry;
+        this.stepRegistry = stepRegistry;
 	}
 
 	/**
 	 * The {@link JobRegistry} to use for jobs created.
-	 * 
-	 * @param jobRegistry
+	 *
+	 * @param jobRegistry the job registry
 	 */
 	public void setJobRegistry(JobRegistry jobRegistry) {
 		this.jobRegistry = jobRegistry;
+        if (stepRegistry == null && jobRegistry instanceof StepRegistry) {
+            setStepRegistry((StepRegistry) jobRegistry);
+        }
 	}
+
+    /**
+     * The {@link StepRegistry} to use for the steps of created jobs.
+     *
+     * @param stepRegistry the step registry
+     */
+    public void setStepRegistry(StepRegistry stepRegistry) {
+        this.stepRegistry = stepRegistry;
+    }
 
 	/**
 	 * Unregister all the jobs and close all the contexts created by this
 	 * loader.
-	 * 
+	 *
 	 * @see JobLoader#clear()
 	 */
 	public void clear() {
@@ -85,6 +119,7 @@ public class DefaultJobLoader implements JobLoader {
 		}
 		for (String jobName : jobRegistry.getJobNames()) {
 			jobRegistry.unregister(jobName);
+            stepRegistry.unregisterStepsFromJob(jobName);
 		}
 		contexts.clear();
 	}
@@ -97,6 +132,7 @@ public class DefaultJobLoader implements JobLoader {
 			for (String name : contextToJobNames.get(context)) {
 				logger.debug("Unregistering job: " + name + " from context: " + context.getDisplayName());
 				jobRegistry.unregister(name);
+                stepRegistry.unregisterStepsFromJob(name);
 			}
 			context.close();
 		}
@@ -105,7 +141,7 @@ public class DefaultJobLoader implements JobLoader {
 			return doLoad(factory, true);
 		}
 		catch (DuplicateJobException e) {
-			throw new IllegalStateException("Found duplicte job in reload (it should have been unregistered "
+			throw new IllegalStateException("Found duplicate job in reload (it should have been unregistered "
 					+ "if it was previously registered in this loader)", e);
 		}
 	}
@@ -151,6 +187,7 @@ public class DefaultJobLoader implements JobLoader {
 				JobFactory jobFactory = new ReferenceJobFactory(job);
 				jobRegistry.register(jobFactory);
 				jobsRegistered.add(jobName);
+                stepRegistry.register(job.getName(), getSteps(job, context));
 
 			}
 
@@ -174,4 +211,43 @@ public class DefaultJobLoader implements JobLoader {
 
 	}
 
+    /**
+     * Returns all the {@link Step} instances defined by the specified {@link Job}.
+     *
+     * @param job the given job
+     * @param jobApplicationContext the application context of the job
+     * @return all the {@link Step} defined in the given job
+     * @see StepLocator
+     */
+    private Collection<Step> getSteps(final Job job, final ApplicationContext jobApplicationContext) {
+        // TODO: that sounds like we need a stronger contract here
+        if (!(job instanceof StepLocator)) {
+            throw new UnsupportedOperationException("Cannot locate step from a Job that is not a StepLocator: job="
+					+ job.getName() + " does not implement StepLocator");
+        }
+        final StepLocator stepLocator = (StepLocator) job;
+        final Collection<String> stepNames = stepLocator.getStepNames();
+        final Collection<Step> result = new ArrayList<Step>();
+        for (String stepName : stepNames) {
+            result.add(stepLocator.getStep(stepName));
+        }
+
+        // Because some steps are referenced by name, we need to look in the context to see if there
+        // are more Step instances defined. Right now they are registered as being available in the
+        // context of the job but we have no idea if they are linked to that Job or not.
+        @SuppressWarnings("unchecked")
+        final Map<String, Step> allSteps = jobApplicationContext.getBeansOfType(Step.class);
+        for (Map.Entry<String, Step> entry : allSteps.entrySet()) {
+           if (!stepNames.contains(entry.getKey())) {
+               result.add(entry.getValue());
+           }
+        }
+        return result;
+    }
+
+    public void afterPropertiesSet() {
+        Assert.notNull(jobRegistry, "Job registry could not be null.");
+        Assert.notNull(stepRegistry, "Step registry could not be null. Should be set if the Job registry " +
+                "implementation does not implement StepRegistry.");
+    }
 }
