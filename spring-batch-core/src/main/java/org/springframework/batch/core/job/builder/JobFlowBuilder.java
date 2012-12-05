e9bcc10646edb5d150177411d5a383b0d04dcdbc
Dave Syer <dsyer@vmware.com>
2012-12-05 14:09:45 +0000

    BATCH-1871: Add flow and job builders

---
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/builder/JobFlowBuilder.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.job.builder;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+public class JobFlowBuilder extends FlowBuilder<FlowJobBuilder> {
+
+	private FlowJobBuilder parent;
+
+	public JobFlowBuilder(FlowJobBuilder parent) {
+		super(parent.getName());
+		this.parent = parent;
+	}
+
+	public JobFlowBuilder(FlowJobBuilder parent, Step step) {
+		super(parent.getName());
+		this.parent = parent;
+		start(step);
+	}
+
+	public JobFlowBuilder(FlowJobBuilder parent, JobExecutionDecider decider) {
+		super(parent.getName());
+		this.parent = parent;
+		start(decider);
+	}
+
+	public JobFlowBuilder(FlowJobBuilder parent, Flow flow) {
+		super(parent.getName());
+		this.parent = parent;
+		start(flow);
+	}
+
+	/**
+	 * Build a flow and inject it into the parent builder. The parent builder is then returned so it can be enhanced
+	 * before building an actual job.  Normally called explicitly via {@link #end()}.
+	 * 
+	 * @see org.springframework.batch.core.job.builder.FlowBuilder#build()
+	 */
+	public FlowJobBuilder build() {
+		Flow flow = flow();
+		parent.flow(flow);
+		return parent;
+	}
+
+}
---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/job/builder/FlowJobBuilderTests.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.job.builder;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInterruptedException;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.UnexpectedJobExecutionException;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.core.step.StepSupport;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+/**
+ * @author Dave Syer
+ * 
+ */
+public class FlowJobBuilderTests {
+
+	private JobRepository jobRepository;
+	
+	private JobExecution execution;
+
+	private StepSupport step1 = new StepSupport("step1") {
+		@Override
+		public void execute(StepExecution stepExecution) throws JobInterruptedException,
+				UnexpectedJobExecutionException {
+			stepExecution.upgradeStatus(BatchStatus.COMPLETED);
+			stepExecution.setExitStatus(ExitStatus.COMPLETED);
+			jobRepository.update(stepExecution);
+		}
+	};
+
+	private StepSupport step2 = new StepSupport("step2") {
+		@Override
+		public void execute(StepExecution stepExecution) throws JobInterruptedException,
+				UnexpectedJobExecutionException {
+			stepExecution.upgradeStatus(BatchStatus.COMPLETED);
+			stepExecution.setExitStatus(ExitStatus.COMPLETED);
+			jobRepository.update(stepExecution);
+		}
+	};
+
+	private StepSupport step3 = new StepSupport("step3") {
+		@Override
+		public void execute(StepExecution stepExecution) throws JobInterruptedException,
+				UnexpectedJobExecutionException {
+			stepExecution.upgradeStatus(BatchStatus.COMPLETED);
+			stepExecution.setExitStatus(ExitStatus.COMPLETED);
+			jobRepository.update(stepExecution);
+		}
+	};
+
+	@Before
+	public void init() throws Exception {
+		jobRepository = new MapJobRepositoryFactoryBean().getJobRepository();
+		execution = jobRepository.createJobExecution("flow", new JobParameters());
+	}
+
+	@Test
+	public void testBuildOnOneLine() throws Exception {
+		FlowJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step1).on("COMPLETED")
+				.to(step2).end().preventRestart();
+		builder.build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildSingleFlow() throws Exception {
+		Flow flow = new FlowBuilder<Flow>("subflow").from(step1).next(step2).build();
+		FlowJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(flow).end().preventRestart();
+		builder.build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildOverTwoLines() throws Exception {
+		FlowJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step1).on("COMPLETED")
+				.to(step2).end();
+		builder.preventRestart();
+		builder.build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildSubflow() throws Exception {
+		Flow flow = new FlowBuilder<Flow>("subflow").from(step1).end();
+		JobFlowBuilder builder = new JobBuilder("flow").repository(jobRepository).start(flow);
+		builder.on("COMPLETED").to(step2);
+		builder.end().preventRestart().build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildSplit() throws Exception {
+		Flow flow = new FlowBuilder<Flow>("subflow").from(step1).end();
+		SimpleJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step2);
+		builder.split(new SimpleAsyncTaskExecutor()).add(flow).end();
+		builder.preventRestart().build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildDecision() throws Exception {
+		JobExecutionDecider decider = new JobExecutionDecider() {
+			private int count = 0;
+			public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+				count++;
+				return count<2 ? new FlowExecutionStatus("ONGOING") : FlowExecutionStatus.COMPLETED;
+			}
+		};
+		step1.setAllowStartIfComplete(true);
+		SimpleJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step1);
+		builder.next(decider).on("COMPLETED").end().from(decider).on("*").to(step1).end();
+		builder.preventRestart().build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildWithIntermediateSimpleJob() throws Exception {
+		SimpleJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step1);
+		builder.on("COMPLETED").to(step2).end();
+		builder.preventRestart();
+		builder.build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+	@Test
+	public void testBuildWithIntermediateSimpleJobTwoSteps() throws Exception {
+		SimpleJobBuilder builder = new JobBuilder("flow").repository(jobRepository).start(step1).next(step2);
+		builder.on("FAILED").to(step3).end();
+		builder.build().execute(execution);
+		assertEquals(BatchStatus.COMPLETED, execution.getStatus());
+		assertEquals(2, execution.getStepExecutions().size());
+	}
+
+}
