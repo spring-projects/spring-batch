071732a3d9d9614a86fe6e7300d3e88ea9bd3997
Will Schipp <wschipp@vmware.com>
2013-01-16 21:35:13 -0500

    BATCH-1745 - removed excessive schema, added test and cleaned up schema mappings

---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/AbstractStepParserTest.java
@@ -0,0 +1,28 @@
+package org.springframework.batch.core.configuration.xml;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.step.AbstractStep;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class AbstractStepParserTest {
+
+	@Resource
+	private ApplicationContext context;
+	
+	@Test
+	public void test() throws Exception {
+		//retrieve the step from the context and see that it's allow is set
+		AbstractStep abstractStep = context.getBean(AbstractStep.class);
+		assertTrue(abstractStep.isAllowStartIfComplete());	
+	}
+
+}
