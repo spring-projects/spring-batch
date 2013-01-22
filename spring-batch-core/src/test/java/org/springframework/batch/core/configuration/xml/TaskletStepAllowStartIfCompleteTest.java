9d53631ce93d34e1270a9134b987092acbb2f369
Michael Minella <mminella@vmware.com>
2013-01-22 13:25:40 -0600

    BATCH-1745: Fixed rebase issue

--- spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/TaskletStepAllowStartIfCompleteTest.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/xml/TaskletStepAllowStartIfCompleteTest.java
@@ -1,6 +1,5 @@
 package org.springframework.batch.core.configuration.xml;
 
-<<<<<<< HEAD
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -16,29 +15,14 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.AbstractStep;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import static org.junit.Assert.assertTrue;
-
-import javax.annotation.Resource;
-
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.batch.core.step.AbstractStep;
->>>>>>> a9edfc18765552d54ac46f721d128260ceaf1e72
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-<<<<<<< HEAD
-=======
-//@Ignore
->>>>>>> a9edfc18765552d54ac46f721d128260ceaf1e72
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 public class TaskletStepAllowStartIfCompleteTest {
 
-<<<<<<< HEAD
 	@Autowired
 	Job job;
 
@@ -69,16 +53,4 @@ public class TaskletStepAllowStartIfCompleteTest {
 		int count = jobRepository.getStepExecutionCount(jobExecution.getJobInstance(), "simpleJob.step1");
 		assertEquals(2, count);
 	}
-=======
-	@Resource
-	private ApplicationContext context;
-	
-	@Test
-	public void test() throws Exception {
-		//retrieve the step from the context and see that it's allow is set
-		AbstractStep abstractStep = context.getBean(AbstractStep.class);
-		assertTrue(abstractStep.isAllowStartIfComplete());	
-	}
-
->>>>>>> a9edfc18765552d54ac46f721d128260ceaf1e72
 }
