4ba866403e7ad4553fe8974de36369657e8f07f0
Michael Minella <mminella@vmware.com>
2013-08-13 21:27:09 -0500

    BATCH-2071: Implementation of JSR-352's requirement for loading batch artifacts via the thread local classloader * Added a BeanFactoryPostProcessor to add bean definitions for batch artifacts referenced only by class name * Updated the BatchPropertyBeanPostProcessor to address scope targets * Moved the registration of the various post processors to a new JsrNamespaceUtils (similar to how they are handled with the core namespace)

--- spring-batch-core/src/main/java/org/springframework/batch/core/jsr/configuration/support/BatchPropertyBeanPostProcessor.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/jsr/configuration/support/BatchPropertyBeanPostProcessor.java
@@ -21,6 +21,7 @@ import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+
 import javax.batch.api.BatchProperty;
 import javax.batch.api.Batchlet;
 import javax.batch.api.chunk.ItemProcessor;
@@ -29,6 +30,7 @@ import javax.batch.api.chunk.ItemWriter;
 import javax.batch.api.chunk.listener.ItemProcessListener;
 import javax.batch.api.chunk.listener.ItemReadListener;
 import javax.batch.api.chunk.listener.ItemWriteListener;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.core.Job;
@@ -50,111 +52,113 @@ import org.springframework.util.ReflectionUtils;
  * @since 3.0
  */
 public class BatchPropertyBeanPostProcessor implements BeanPostProcessor {
-    @Autowired
-    private BatchPropertyContext batchPropertyContext;
-    private Log logger = LogFactory.getLog(getClass());
-    private Set<Class<? extends Annotation>> requiredAnnotations = new HashSet<Class<? extends Annotation>>();
+	@Autowired
+	private BatchPropertyContext batchPropertyContext;
+	private Log logger = LogFactory.getLog(getClass());
+	private Set<Class<? extends Annotation>> requiredAnnotations = new HashSet<Class<? extends Annotation>>();
 
-    public BatchPropertyBeanPostProcessor() {
-        setRequiredAnnotations();
-    }
+	public BatchPropertyBeanPostProcessor() {
+		setRequiredAnnotations();
+	}
 
-    @Override
-    public Object postProcessBeforeInitialization(final Object bean, String beanName) throws BeansException {
-        if (!isBatchArtifact(bean)) {
-            return bean;
-        }
+	@Override
+	public Object postProcessBeforeInitialization(final Object bean, String beanName) throws BeansException {
+		if (!isBatchArtifact(bean)) {
+			return bean;
+		}
 
-        final Properties artifactProperties = batchPropertyContext.getBatchProperties(beanName);
+		final Properties artifactProperties = batchPropertyContext.getBatchProperties(beanName);
 
-        if (artifactProperties.isEmpty()) {
-            return bean;
-        }
+		if (artifactProperties.isEmpty()) {
+			return bean;
+		}
 
-        injectBatchProperties(bean, artifactProperties);
+		injectBatchProperties(bean, artifactProperties);
 
-        return bean;
-    }
+		return bean;
+	}
 
-    private void setRequiredAnnotations() {
-        ClassLoader cl = BatchPropertyBeanPostProcessor.class.getClassLoader();
+	@SuppressWarnings("unchecked")
+	private void setRequiredAnnotations() {
+		ClassLoader cl = BatchPropertyBeanPostProcessor.class.getClassLoader();
 
-        try {
-            this.requiredAnnotations.add((Class<? extends Annotation>) cl.loadClass("javax.inject.Inject"));
-        } catch (ClassNotFoundException ex) {
-            logger.warn("javax.inject.Inject not found - @BatchProperty marked fields will not be processed.");
-        }
+		try {
+			this.requiredAnnotations.add((Class<? extends Annotation>) cl.loadClass("javax.inject.Inject"));
+		} catch (ClassNotFoundException ex) {
+			logger.warn("javax.inject.Inject not found - @BatchProperty marked fields will not be processed.");
+		}
 
-        this.requiredAnnotations.add(BatchProperty.class);
-    }
+		this.requiredAnnotations.add(BatchProperty.class);
+	}
 
-    private boolean isBatchArtifact(Object bean) {
-        return (bean instanceof ItemReader) ||
-                (bean instanceof ItemProcessor) ||
-                (bean instanceof ItemWriter) ||
-                (bean instanceof CompletionPolicy) ||
-                (bean instanceof Batchlet) ||
-                (bean instanceof ItemReadListener) ||
-                (bean instanceof ItemProcessListener) ||
-                (bean instanceof ItemWriteListener) ||
-                (bean instanceof JobExecutionDecider) ||
-                (bean instanceof Step) ||
-                (bean instanceof Job);
-    }
+	private boolean isBatchArtifact(Object bean) {
+		return (bean instanceof ItemReader) ||
+				(bean instanceof ItemProcessor) ||
+				(bean instanceof ItemWriter) ||
+				(bean instanceof CompletionPolicy) ||
+				(bean instanceof Batchlet) ||
+				(bean instanceof ItemReadListener) ||
+				(bean instanceof ItemProcessListener) ||
+				(bean instanceof ItemWriteListener) ||
+				(bean instanceof JobExecutionDecider) ||
+				(bean instanceof Step) ||
+				(bean instanceof Job);
+	}
 
-    private void injectBatchProperties(final Object bean, final Properties artifactProperties) {
-        ReflectionUtils.doWithFields(bean.getClass(), new ReflectionUtils.FieldCallback() {
-            public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-                if (isValidFieldModifier(field) && isAnnotated(field)) {
-                    boolean isAccessible = field.isAccessible();
-                    field.setAccessible(true);
+	private void injectBatchProperties(final Object bean, final Properties artifactProperties) {
+		ReflectionUtils.doWithFields(bean.getClass(), new ReflectionUtils.FieldCallback() {
+			@Override
+			public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+				if (isValidFieldModifier(field) && isAnnotated(field)) {
+					boolean isAccessible = field.isAccessible();
+					field.setAccessible(true);
 
-                    String batchProperty = getBatchPropertyFieldValue(field, artifactProperties);
+					String batchProperty = getBatchPropertyFieldValue(field, artifactProperties);
 
-                    if (batchProperty != null) {
-                        field.set(bean, batchProperty);
-                    }
+					if (batchProperty != null) {
+						field.set(bean, batchProperty);
+					}
 
-                    field.setAccessible(isAccessible);
-                }
-            }
-        });
-    }
+					field.setAccessible(isAccessible);
+				}
+			}
+		});
+	}
 
-    private String getBatchPropertyFieldValue(Field field, Properties batchArtifactProperties) {
-        BatchProperty batchProperty = field.getAnnotation(BatchProperty.class);
+	private String getBatchPropertyFieldValue(Field field, Properties batchArtifactProperties) {
+		BatchProperty batchProperty = field.getAnnotation(BatchProperty.class);
 
-        if (!"".equals(batchProperty.name())) {
-            return getBatchProperty(batchProperty.name(), batchArtifactProperties);
-        }
+		if (!"".equals(batchProperty.name())) {
+			return getBatchProperty(batchProperty.name(), batchArtifactProperties);
+		}
 
-        return getBatchProperty(field.getName(), batchArtifactProperties);
-    }
+		return getBatchProperty(field.getName(), batchArtifactProperties);
+	}
 
-    private String getBatchProperty(String propertyKey, Properties batchArtifactProperties) {
-        if (batchArtifactProperties.containsKey(propertyKey)) {
-            return (String) batchArtifactProperties.get(propertyKey);
-        }
+	private String getBatchProperty(String propertyKey, Properties batchArtifactProperties) {
+		if (batchArtifactProperties.containsKey(propertyKey)) {
+			return (String) batchArtifactProperties.get(propertyKey);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private boolean isAnnotated(Field field) {
-        for (Class<? extends Annotation> annotation : requiredAnnotations) {
-            if(!field.isAnnotationPresent(annotation)) {
-                return false;
-            }
-        }
+	private boolean isAnnotated(Field field) {
+		for (Class<? extends Annotation> annotation : requiredAnnotations) {
+			if(!field.isAnnotationPresent(annotation)) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private boolean isValidFieldModifier(Field field) {
-        return !Modifier.isStatic(field.getModifiers()) && !Modifier.isFinal(field.getModifiers());
-    }
+	private boolean isValidFieldModifier(Field field) {
+		return !Modifier.isStatic(field.getModifiers()) && !Modifier.isFinal(field.getModifiers());
+	}
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return bean;
-    }
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		return bean;
+	}
 }
---
+++ spring-batch-core/src/test/java/org/springframework/batch/core/jsr/configuration/xml/ThreadLocalClassloaderBeanPostProcessorTestsBatchlet.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.jsr.configuration.xml;
+
+import javax.batch.api.BatchProperty;
+import javax.batch.api.Batchlet;
+import javax.batch.runtime.context.JobContext;
+import javax.batch.runtime.context.StepContext;
+import javax.inject.Inject;
+
+import org.springframework.util.Assert;
+
+public class ThreadLocalClassloaderBeanPostProcessorTestsBatchlet implements Batchlet {
+	@Inject
+	@BatchProperty
+	public String jobParam1;
+
+	@Inject
+	public JobContext jobContext;
+
+	@Inject
+	public StepContext stepContext;
+
+	@Override
+	public String process() throws Exception {
+		Assert.isTrue("someParameter".equals(jobParam1), jobParam1 + " does not equal someParamter");
+		Assert.isTrue("threadLocalClassloaderBeanPostProcessorTestsJob".equals(jobContext.getJobName()));
+		Assert.isTrue("step1".equals(stepContext.getStepName()));
+
+		return null;
+	}
+
+	@Override
+	public void stop() throws Exception {
+	}
+}
