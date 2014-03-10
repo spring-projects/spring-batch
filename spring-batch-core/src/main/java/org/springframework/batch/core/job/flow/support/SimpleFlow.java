4e5b4c12baa6cc918a7bd8fca61ded338ce28a02
Michael Minella <mminella@gopivotal.com>
2014-03-10 10:47:56 -0500

    BATCH-2016: Added new flag to indicate if the step is being executed on a restart.  If it is, the transition will be followed.  If not, we'll look to see if we should be looking for somewhere else to restart.

--- spring-batch-core/src/main/java/org/springframework/batch/core/job/flow/support/SimpleFlow.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/flow/support/SimpleFlow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2013 the original author or authors.
+ * Copyright 2006-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,8 @@
  */
 package org.springframework.batch.core.job.flow.support;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.batch.core.JobExecutionException;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.job.flow.Flow;
@@ -37,6 +26,16 @@ import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.FlowExecutor;
 import org.springframework.batch.core.job.flow.State;
 import org.springframework.beans.factory.InitializingBean;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * A {@link Flow} that branches conditionally depending on the exit status of
@@ -159,7 +158,7 @@ public class SimpleFlow implements Flow, InitializingBean {
 			catch (Exception e) {
 				executor.close(new FlowExecution(stateName, status));
 				throw new FlowExecutionException(String.format("Ended flow=%s at state=%s with exception", name,
-						stateName), e);
+																	  stateName), e);
 			}
 
 			logger.debug("Completed state="+stateName+" with status="+status);
@@ -180,8 +179,9 @@ public class SimpleFlow implements Flow, InitializingBean {
 
 		if(stepExecution != null) {
 			Boolean reRun = (Boolean) stepExecution.getExecutionContext().get("batch.restart");
+			Boolean executed = (Boolean) stepExecution.getExecutionContext().get("batch.executed");
 
-			if(reRun != null && reRun && status == FlowExecutionStatus.STOPPED && !state.getName().endsWith(stepExecution.getStepName())) {
+			if((executed == null || !executed) && reRun != null && reRun && status == FlowExecutionStatus.STOPPED && !state.getName().endsWith(stepExecution.getStepName())) {
 				continued = true;
 			}
 		}
@@ -191,7 +191,7 @@ public class SimpleFlow implements Flow, InitializingBean {
 
 	/**
 	 * @return the next {@link Step} (or null if this is the end)
-	 * @throws JobExecutionException
+	 * @throws org.springframework.batch.core.job.flow.FlowExecutionException
 	 */
 	private State nextState(String stateName, FlowExecutionStatus status) throws FlowExecutionException {
 
@@ -199,7 +199,7 @@ public class SimpleFlow implements Flow, InitializingBean {
 
 		if (set == null) {
 			throw new FlowExecutionException(String.format("No transitions found in flow=%s for state=%s", getName(),
-					stateName));
+																  stateName));
 		}
 
 		String next = null;
@@ -216,18 +216,15 @@ public class SimpleFlow implements Flow, InitializingBean {
 		}
 
 		if (next == null) {
-			throw new FlowExecutionException(String.format(
-					"Next state not found in flow=%s for state=%s with exit status=%s", getName(), stateName, status.getName()));
+			throw new FlowExecutionException(String.format("Next state not found in flow=%s for state=%s with exit status=%s", getName(), stateName, status.getName()));
 		}
 
 		if (!stateMap.containsKey(next)) {
 			throw new FlowExecutionException(String.format("Next state not specified in flow=%s for next=%s",
-					getName(), next));
+																  getName(), next));
 		}
 
-		State state = stateMap.get(next);
-
-		return state;
+		return stateMap.get(next);
 
 	}
 
@@ -281,7 +278,7 @@ public class SimpleFlow implements Flow, InitializingBean {
 
 		if (!hasEndStep) {
 			throw new IllegalArgumentException(
-					"No end state was found.  You must specify at least one transition with no next state.");
+													  "No end state was found.  You must specify at least one transition with no next state.");
 		}
 
 		startState = stateTransitions.get(0).getState();
--- spring-batch-core/src/test/java/org/springframework/batch/core/step/RestartInPriorStepTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/step/RestartInPriorStepTests.java
@@ -1,10 +1,20 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.batch.core.step;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Map;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
@@ -26,6 +36,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Michael Minella
  *
@@ -46,26 +60,13 @@ public class RestartInPriorStepTests {
 	@Autowired
 	private Job job;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
-
 	@Test
 	public void test() throws Exception {
-		//
-		// Run 1
-		//
 		JobExecution run1 = jobLauncher.run(job, new JobParameters());
 
 		assertEquals(BatchStatus.STOPPED, run1.getStatus());
 		assertEquals(2, run1.getStepExecutions().size());
 
-		//
-		// Run 2
-		//
 		JobExecution run2 = jobLauncher.run(job, new JobParameters());
 
 		assertEquals(BatchStatus.COMPLETED, run2.getStatus());
