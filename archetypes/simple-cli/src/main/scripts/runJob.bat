5600061b92a62d14e72c58b05661f94cece0dedf
Michael Minella <mminella@gopivotal.com>
2014-04-30 12:11:21 -0500

    BATCH-2223: Removed 'archetypes' in favor of Spring Boot based start up

--- archetypes/simple-cli/src/main/scripts/runJob.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-IF NOT DEFINED JAVA_HOME (
-    echo Error: JAVA_HOME environment variable is not set.
-    EXIT /B
-) 
-
-%JAVA_HOME%\bin\java -cp resources\;lib\* org.springframework.batch.core.launch.support.CommandLineJobRunner classpath:/launch-context.xml personJob
--- archetypes/simple-cli/src/main/scripts/runJob.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$JAVA_HOME" = "" ]; then
-  echo "Error: JAVA_HOME environment variable is not set."
-  exit 1
-fi
-
-$JAVA_HOME/bin/java -cp resources/:lib/* org.springframework.batch.core.launch.support.CommandLineJobRunner classpath:/launch-context.xml personJob
