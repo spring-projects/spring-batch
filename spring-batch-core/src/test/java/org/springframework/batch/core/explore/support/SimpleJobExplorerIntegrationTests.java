6d94ae9031f9df1a349e628e55c52c1515eacd0f
Michael Minella <mminella@gopivotal.com>
2014-05-05 15:03:53 -0500

    Updated hsqldb urls to use MVCC mode

--- spring-batch-core/src/test/java/org/springframework/batch/core/explore/support/SimpleJobExplorerIntegrationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/explore/support/SimpleJobExplorerIntegrationTests.java
@@ -111,7 +111,7 @@ public class SimpleJobExplorerIntegrationTests {
 		public BasicDataSource dataSource() {
 			BasicDataSource dataSource = new BasicDataSource();
 			dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-			dataSource.setUrl("jdbc:hsqldb:mem:testdb;sql.enforce_strict_size=true");
+			dataSource.setUrl("jdbc:hsqldb:mem:testdb;sql.enforce_strict_size=true;hsqldb.tx=mvcc");
 			dataSource.setUsername("sa");
 			dataSource.setPassword("");
 			return dataSource;
