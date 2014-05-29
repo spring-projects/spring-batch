676b9db3d89a04a9a228e11aa39d14b593e5c997
Chris Schaefer <cschaefer@gopivotal.com>
2014-05-29 13:04:50 -0400

    BATCH-2242: Stopping a job in STARTING state throws OptimisticLockingFailureException

--- spring-batch-core/src/main/java/org/springframework/batch/core/repository/support/SimpleJobRepository.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/repository/support/SimpleJobRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2013 the original author or authors.
+ * Copyright 2006-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -157,6 +157,8 @@ public class SimpleJobRepository implements JobRepository {
 		Assert.notNull(jobExecution.getId(), "JobExecution must be already saved (have an id assigned).");
 
 		jobExecution.setLastUpdated(new Date(System.currentTimeMillis()));
+
+		jobExecutionDao.synchronizeStatus(jobExecution);
 		jobExecutionDao.updateJobExecution(jobExecution);
 	}
 
