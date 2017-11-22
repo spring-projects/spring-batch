5d883a3e61916ea67d2f5d33a3e2ca80e931dbbc
Michael Minella <mminella@pivotal.io>
2017-11-22 09:19:03 -0600

    Fixed javadoc for `JobOperator#abandon`
    The javadoc in the `JobOperator#abandon` indicated that a JobExecution
    marked as `ABANDONED` can be restarted (it cannot).
    
    Resolves BATCH-2654

--- spring-batch-core/src/main/java/org/springframework/batch/core/launch/JobOperator.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/launch/JobOperator.java
@@ -197,17 +197,17 @@ public interface JobOperator {
 	 */
 	Set<String> getJobNames();
 
-    /**
-     * Mark the {@link JobExecution} as ABANDONED. If a stop signal is ignored
-     * because the process died this is the best way to mark a job as finished
-     * with (as opposed to STOPPED). An abandoned job execution can be
-     * restarted, but a stopping one cannot.
-     *
-     * @param jobExecutionId the job execution id to abort
-     * @return the {@link JobExecution} that was aborted
-     * @throws NoSuchJobExecutionException
-     * @throws JobExecutionAlreadyRunningException if the job is running (it
-     * should be stopped first)
-     */
-    JobExecution abandon(long jobExecutionId) throws NoSuchJobExecutionException, JobExecutionAlreadyRunningException;
+	/**
+	 * Mark the {@link JobExecution} as ABANDONED. If a stop signal is ignored
+	 * because the process died this is the best way to mark a job as finished
+	 * with (as opposed to STOPPED). An abandoned job execution cannot be
+	 * restarted by the framework.
+	 *
+	 * @param jobExecutionId the job execution id to abort
+	 * @return the {@link JobExecution} that was aborted
+	 * @throws NoSuchJobExecutionException thrown if there is no job execution for the jobExecutionId.
+	 * @throws JobExecutionAlreadyRunningException if the job is running (it
+	 * should be stopped first)
+	 */
+	JobExecution abandon(long jobExecutionId) throws NoSuchJobExecutionException, JobExecutionAlreadyRunningException;
 }
