d69e46ec29518e88626d3254e78b768d3b836411
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-05-10 23:07:38 +0200

    Add a remote chunking sample
    This commit adds a self contained remote chunking sample that uses
    an embedded JMS broker (for simplicity). Even though the broker is
    embedded, communication between the master and workers is still done
    through JMS queues and Spring Integration channels and messages are
    sent over the wire through a TCP port.
    
    This commit also fixes a few typos in the documentation section about
    remote chunking.
    
    Resolves BATCH-2721

---
+++ spring-batch-samples/src/main/java/org/springframework/batch/sample/remotechunking/MasterConfiguration.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.sample.remotechunking;
+
+import java.util.Arrays;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.integration.chunk.ChunkMessageChannelItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.jms.dsl.Jms;
+
+/**
+ * This configuration class is for the master side of the remote chunking sample.
+ * The master step reads numbers from 1 to 6 and sends 2 chunks {1, 2, 3} and
+ * {4, 5, 6} to workers for processing and writing.
+ *
+ * @author Mahmoud Ben Hassine
+ */
+@Configuration
+@EnableBatchProcessing
+@EnableIntegration
+@PropertySource("classpath:remote-chunking.properties")
+public class MasterConfiguration {
+
+	@Value("${broker.url}")
+	private String brokerUrl;
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public ActiveMQConnectionFactory connectionFactory() {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+		connectionFactory.setBrokerURL(this.brokerUrl);
+		connectionFactory.setTrustAllPackages(true);
+		return connectionFactory;
+	}
+
+	/*
+	 * Configure outbound flow (requests going to workers)
+	 */
+	@Bean
+	public DirectChannel requests() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public IntegrationFlow outboundFlow(ActiveMQConnectionFactory connectionFactory) {
+		return IntegrationFlows
+				.from(requests())
+				.handle(Jms.outboundAdapter(connectionFactory).destination("requests"))
+				.get();
+	}
+
+	/*
+	 * Configure inbound flow (replies coming from workers)
+	 */
+	@Bean
+	public QueueChannel replies() {
+		return new QueueChannel();
+	}
+
+	@Bean
+	public IntegrationFlow inboundFlow(ActiveMQConnectionFactory connectionFactory) {
+		return IntegrationFlows
+				.from(Jms.messageDrivenChannelAdapter(connectionFactory).destination("replies"))
+				.channel(replies())
+				.get();
+	}
+
+	/*
+	 * Configure master step components
+	 */
+	@Bean
+	public ListItemReader<Integer> itemReader() {
+		return new ListItemReader<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+	}
+
+	@Bean
+	public ChunkMessageChannelItemWriter<Integer> itemWriter() {
+		MessagingTemplate messagingTemplate = new MessagingTemplate();
+		messagingTemplate.setDefaultChannel(requests());
+		ChunkMessageChannelItemWriter<Integer> chunkMessageChannelItemWriter
+				= new ChunkMessageChannelItemWriter<>();
+		chunkMessageChannelItemWriter.setMessagingOperations(messagingTemplate);
+		chunkMessageChannelItemWriter.setReplyChannel(replies());
+		return chunkMessageChannelItemWriter;
+	}
+
+	@Bean
+	public TaskletStep masterStep() {
+		return this.stepBuilderFactory.get("masterStep")
+				.<Integer, Integer>chunk(3)
+				.reader(itemReader())
+				.writer(itemWriter())
+				.build();
+	}
+
+	@Bean
+	public Job remoteChunkingJob() {
+		return this.jobBuilderFactory.get("remoteChunkingJob")
+				.start(masterStep())
+				.build();
+	}
+
+}
