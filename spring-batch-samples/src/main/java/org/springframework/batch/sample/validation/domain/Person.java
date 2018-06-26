9f2b752f3f31a9edb3fb97a08d713a28efb6bbe2
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-06-26 16:21:09 +0200

    Add a validator based on the Bean Validation API
    Resolves BATCH-2690

---
+++ spring-batch-samples/src/main/java/org/springframework/batch/sample/validation/domain/Person.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.batch.sample.validation.domain;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author Mahmoud Ben Hassine
+ */
+public class Person {
+
+	private int id;
+
+	@NotEmpty
+	private String name;
+
+	public Person() {
+	}
+
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
