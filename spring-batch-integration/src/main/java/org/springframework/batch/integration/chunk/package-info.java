1f2a4bde13d47457b41c31c20e18f91196083049
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-07-17 10:59:25 +0200

    Add JSR-305 annotations to public APIs
    (Partially) Resolves BATCH-2688

--- spring-batch-integration/src/main/java/org/springframework/batch/integration/chunk/package-info.java
+++ spring-batch-integration/src/main/java/org/springframework/batch/integration/chunk/package-info.java
@@ -2,5 +2,9 @@
  * Components for remote chunking.
  *
  * @author Michael Minella
+ * @author Mahmoud Ben Hassine
  */
+@NonNullApi
 package org.springframework.batch.integration.chunk;
+
+import org.springframework.lang.NonNullApi;
--- spring-batch-integration/src/main/java/org/springframework/batch/integration/partition/package-info.java
+++ spring-batch-integration/src/main/java/org/springframework/batch/integration/partition/package-info.java
@@ -2,5 +2,9 @@
  * Remote partitioning components.
  *
  * @author Michael Minella
+ * @author Mahmoud Ben Hassine
  */
+@NonNullApi
 package org.springframework.batch.integration.partition;
+
+import org.springframework.lang.NonNullApi;
