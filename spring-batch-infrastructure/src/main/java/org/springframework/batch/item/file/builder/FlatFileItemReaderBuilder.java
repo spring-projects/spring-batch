fe1aef3bab83c758dd7a0ac3ef591456010e8ce9
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-08-09 12:16:36 +0200

    Allow whitespace characters as delimiters in FlatFileItemReader
    Resolves BATCH-2738

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemReaderBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemReaderBuilder.java
@@ -628,7 +628,7 @@ public class FlatFileItemReaderBuilder<T> {
 
 			tokenizer.setNames(this.names.toArray(new String[this.names.size()]));
 
-			if(StringUtils.hasText(this.delimiter)) {
+			if(StringUtils.hasLength(this.delimiter)) {
 				tokenizer.setDelimiter(this.delimiter);
 			}
 
