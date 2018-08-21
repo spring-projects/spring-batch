e83ed1db1109264282df452b3a15d4c4b625bcd7
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-08-21 23:59:03 +0200

    Add convenience methods in FlatFileItemWriterBuilder to generate delimited files
    Resolves BATCH-2696

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/builder/FlatFileItemWriterBuilderTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/builder/FlatFileItemWriterBuilderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * @author Michael Minella
+ * @author Mahmoud Ben Hassine
  */
 public class FlatFileItemWriterBuilderTests {
 
@@ -45,6 +46,22 @@ public class FlatFileItemWriterBuilderTests {
 	@Test(expected = IllegalArgumentException.class)
 	public void testMissingLineAggregator() {
 		new FlatFileItemWriterBuilder<Foo>()
+				.build();
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testMultipleLineAggregators() throws IOException {
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		new FlatFileItemWriterBuilder<Foo>()
+				.name("itemWriter")
+				.resource(output)
+				.delimited()
+				.delimiter(";")
+				.names(new String[] {"foo", "bar"})
+				.formatted()
+				.format("%2s%2s")
+				.names(new String[] {"foo", "bar"})
 				.build();
 	}
 
@@ -72,6 +89,145 @@ public class FlatFileItemWriterBuilderTests {
 		writer.close();
 
 		assertEquals("HEADER$Foo{first=1, second=2, third='3'}$Foo{first=4, second=5, third='6'}$FOOTER", readLine("UTF-16LE", output));
+	}
+
+	@Test
+	public void testDelimitedOutputWithDefaultDelimiter() throws Exception {
+
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		FlatFileItemWriter<Foo> writer = new FlatFileItemWriterBuilder<Foo>()
+				.name("foo")
+				.resource(output)
+				.lineSeparator("$")
+				.delimited()
+				.names(new String[] {"first", "second", "third"})
+				.encoding("UTF-16LE")
+				.headerCallback(writer1 -> writer1.append("HEADER"))
+				.footerCallback(writer12 -> writer12.append("FOOTER"))
+				.build();
+
+		ExecutionContext executionContext = new ExecutionContext();
+
+		writer.open(executionContext);
+
+		writer.write(Arrays.asList(new Foo(1, 2, "3"), new Foo(4, 5, "6")));
+
+		writer.close();
+
+		assertEquals("HEADER$1,2,3$4,5,6$FOOTER", readLine("UTF-16LE", output));
+	}
+
+	@Test
+	public void testDelimitedOutputWithDefaultFieldExtractor() throws Exception {
+
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		FlatFileItemWriter<Foo> writer = new FlatFileItemWriterBuilder<Foo>()
+				.name("foo")
+				.resource(output)
+				.lineSeparator("$")
+				.delimited()
+				.delimiter(";")
+				.names(new String[] {"first", "second", "third"})
+				.encoding("UTF-16LE")
+				.headerCallback(writer1 -> writer1.append("HEADER"))
+				.footerCallback(writer12 -> writer12.append("FOOTER"))
+				.build();
+
+		ExecutionContext executionContext = new ExecutionContext();
+
+		writer.open(executionContext);
+
+		writer.write(Arrays.asList(new Foo(1, 2, "3"), new Foo(4, 5, "6")));
+
+		writer.close();
+
+		assertEquals("HEADER$1;2;3$4;5;6$FOOTER", readLine("UTF-16LE", output));
+	}
+
+	@Test
+	public void testDelimitedOutputWithCustomFieldExtractor() throws Exception {
+
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		FlatFileItemWriter<Foo> writer = new FlatFileItemWriterBuilder<Foo>()
+				.name("foo")
+				.resource(output)
+				.lineSeparator("$")
+				.delimited()
+				.delimiter(" ")
+				.fieldExtractor(item -> new Object[] {item.getFirst(), item.getThird()})
+				.encoding("UTF-16LE")
+				.headerCallback(writer1 -> writer1.append("HEADER"))
+				.footerCallback(writer12 -> writer12.append("FOOTER"))
+				.build();
+
+		ExecutionContext executionContext = new ExecutionContext();
+
+		writer.open(executionContext);
+
+		writer.write(Arrays.asList(new Foo(1, 2, "3"), new Foo(4, 5, "6")));
+
+		writer.close();
+
+		assertEquals("HEADER$1 3$4 6$FOOTER", readLine("UTF-16LE", output));
+	}
+
+	@Test
+	public void testFormattedOutputWithDefaultFieldExtractor() throws Exception {
+
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		FlatFileItemWriter<Foo> writer = new FlatFileItemWriterBuilder<Foo>()
+				.name("foo")
+				.resource(output)
+				.lineSeparator("$")
+				.formatted()
+				.format("%2s%2s%2s")
+				.names(new String[] {"first", "second", "third"})
+				.encoding("UTF-16LE")
+				.headerCallback(writer1 -> writer1.append("HEADER"))
+				.footerCallback(writer12 -> writer12.append("FOOTER"))
+				.build();
+
+		ExecutionContext executionContext = new ExecutionContext();
+
+		writer.open(executionContext);
+
+		writer.write(Arrays.asList(new Foo(1, 2, "3"), new Foo(4, 5, "6")));
+
+		writer.close();
+
+		assertEquals("HEADER$ 1 2 3$ 4 5 6$FOOTER", readLine("UTF-16LE", output));
+	}
+
+	@Test
+	public void testFormattedOutputWithCustomFieldExtractor() throws Exception {
+
+		Resource output = new FileSystemResource(File.createTempFile("foo", "txt"));
+
+		FlatFileItemWriter<Foo> writer = new FlatFileItemWriterBuilder<Foo>()
+				.name("foo")
+				.resource(output)
+				.lineSeparator("$")
+				.formatted()
+				.format("%3s%3s")
+				.fieldExtractor(item -> new Object[] {item.getFirst(), item.getThird()})
+				.encoding("UTF-16LE")
+				.headerCallback(writer1 -> writer1.append("HEADER"))
+				.footerCallback(writer12 -> writer12.append("FOOTER"))
+				.build();
+
+		ExecutionContext executionContext = new ExecutionContext();
+
+		writer.open(executionContext);
+
+		writer.write(Arrays.asList(new Foo(1, 2, "3"), new Foo(4, 5, "6")));
+
+		writer.close();
+
+		assertEquals("HEADER$  1  3$  4  6$FOOTER", readLine("UTF-16LE", output));
 	}
 
 	@Test
