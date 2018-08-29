caa3befaeb179ad5d33c08720b218db8dd030a2f
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-08-29 16:30:11 +0200

    Fix typo in method name HibernatePagingItemReaderBuilder#useSatelessSession
    Resolves BATCH-2745

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/builder/HibernatePagingItemReaderBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/builder/HibernatePagingItemReaderBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ import org.springframework.util.StringUtils;
  *
  * @author Michael Minella
  * @author Glenn Renfro
+ * @author Mahmoud Ben Hassine
  * @since 4.0
  * @see HibernatePagingItemReader
  */
@@ -223,8 +224,24 @@ public class HibernatePagingItemReaderBuilder<T> {
 	 * @param useStatelessSession Defaults to false
 	 * @return this instance for method chaining
 	 * @see HibernatePagingItemReader#setUseStatelessSession(boolean)
+	 * @deprecated This method is deprecated in favor of
+	 * {@link HibernatePagingItemReaderBuilder#useStatelessSession} and will be
+	 * removed in version 4.2.
 	 */
+	@Deprecated
 	public HibernatePagingItemReaderBuilder<T> useSatelessSession(boolean useStatelessSession) {
+		return useStatelessSession(useStatelessSession);
+	}
+
+	/**
+	 * Indicator for whether to use a {@link org.hibernate.StatelessSession}
+	 * (<code>true</code>) or a {@link org.hibernate.Session} (<code>false</code>).
+	 *
+	 * @param useStatelessSession Defaults to false
+	 * @return this instance for method chaining
+	 * @see HibernatePagingItemReader#setUseStatelessSession(boolean)
+	 */
+	public HibernatePagingItemReaderBuilder<T> useStatelessSession(boolean useStatelessSession) {
 		this.statelessSession = useStatelessSession;
 
 		return this;
