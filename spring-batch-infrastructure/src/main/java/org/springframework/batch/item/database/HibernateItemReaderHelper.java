a591008eba4cd62618d5a8d8f167836b4140c67b
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-09-18 18:45:07 +0200

    Upgrade dependencies for Boot 2.1

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/HibernateItemReaderHelper.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/HibernateItemReaderHelper.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.query.Query;
+import org.hibernate.Query;
 import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
