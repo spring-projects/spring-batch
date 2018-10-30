f0787454f875d6b71869ba69d023d2d731206139
Dimitrios Liapis <dimitrisli@gmail.com>
2018-10-30 23:33:09 +0100

    Fix thread usage in SynchronizedItemStreamReaderTests
    along with its builder test class SynchronizedItemStreamReaderBuilderTests
    
    Resolves #837

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/support/SynchronizedItemStreamReaderTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/support/SynchronizedItemStreamReaderTests.java
@@ -15,131 +15,33 @@
  */
 package org.springframework.batch.item.support;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
-import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.ItemStreamReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.lang.Nullable;
+import org.springframework.beans.factory.InitializingBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Matthew Ouyang
  * @author Mahmoud Ben Hassine
+ * @author Dimitrios Liapis
  *
  */
-class SynchronizedItemStreamReaderTests {
+public class SynchronizedItemStreamReaderTests extends AbstractSynchronizedItemStreamReaderTests {
 
-	/**
-	 * A simple class used to test the SynchronizedItemStreamReader. It simply returns the
-	 * number of times the read method has been called, manages some state variables and
-	 * updates an ExecutionContext.
-	 *
-	 * @author Matthew Ouyang
-	 *
-	 */
-	private class TestItemReader extends AbstractItemStreamItemReader<Integer> implements ItemStreamReader<Integer> {
-
-		private int cursor = 0;
-
-		private boolean isClosed = false;
-
-		public static final String HAS_BEEN_OPENED = "hasBeenOpened";
-
-		public static final String UPDATE_COUNT_KEY = "updateCount";
-
-		@Nullable
-		public Integer read() throws Exception, ParseException, NonTransientResourceException {
-			cursor = cursor + 1;
-			return cursor;
-		}
-
-		public void close() {
-			this.isClosed = true;
-		}
-
-		public void open(ExecutionContext executionContext) {
-			this.isClosed = false;
-			executionContext.put(HAS_BEEN_OPENED, true);
-			executionContext.remove(UPDATE_COUNT_KEY);
-		}
-
-		public void update(ExecutionContext executionContext) {
-
-			if (!executionContext.containsKey(UPDATE_COUNT_KEY)) {
-				executionContext.putInt(UPDATE_COUNT_KEY, 0);
-			}
-
-			executionContext.putInt(UPDATE_COUNT_KEY, executionContext.getInt(UPDATE_COUNT_KEY) + 1);
-		}
-
-		public boolean isClosed() {
-			return this.isClosed;
-		}
-
+	@Override
+	protected SynchronizedItemStreamReader<Object> createNewSynchronizedItemStreamReader() {
+		SynchronizedItemStreamReader<Object> synchronizedItemStreamReader = new SynchronizedItemStreamReader<>();
+		synchronizedItemStreamReader.setDelegate(delegate);
+		return synchronizedItemStreamReader;
 	}
 
 	@Test
-	void testMultipleThreads() throws Exception {
-
-		// Initialized an ExecutionContext and a SynchronizedItemStreamReader to test.
-		final ExecutionContext executionContext = new ExecutionContext();
-
-		final TestItemReader testItemReader = new TestItemReader();
-		final SynchronizedItemStreamReader<Integer> synchronizedItemStreamReader = new SynchronizedItemStreamReader<>();
-		synchronizedItemStreamReader.setDelegate(testItemReader);
-
-		// Open the ItemReader and make sure it's initialized properly.
-		synchronizedItemStreamReader.open(executionContext);
-		assertEquals(true, executionContext.get(TestItemReader.HAS_BEEN_OPENED));
-		assertFalse(testItemReader.isClosed());
-
-		/*
-		 * Set up SIZE threads that read from the reader and updates the execution
-		 * context.
-		 */
-		final Set<Integer> ecSet = new HashSet<>();
-		final int SIZE = 20;
-		Thread[] threads = new Thread[SIZE];
-		for (int i = 0; i < SIZE; i++) {
-			threads[i] = new Thread() {
-				public void run() {
-					try {
-						ecSet.add(synchronizedItemStreamReader.read());
-						synchronizedItemStreamReader.update(executionContext);
-					}
-					catch (Exception ignore) {
-					}
-				}
-			};
-		}
-
-		// Start the threads and block until all threads are done.
-		for (Thread thread : threads) {
-			thread.run();
-		}
-		for (Thread thread : threads) {
-			thread.join();
-		}
-		testItemReader.close();
-
-		/*
-		 * Ensure cleanup happens as expected: status variable is set correctly and
-		 * ExecutionContext variable is set properly. Lastly, the Set<Integer> should have
-		 * 1 to 20 which may not always be the case if the read is not synchronized.
-		 */
-		for (int i = 1; i <= SIZE; i++) {
-			assertTrue(ecSet.contains(i));
-		}
-		assertTrue(testItemReader.isClosed());
-		assertEquals(SIZE, executionContext.getInt(TestItemReader.UPDATE_COUNT_KEY));
+	void testDelegateIsNotNullWhenPropertiesSet() {
+		final Exception expectedException = assertThrows(IllegalStateException.class,
+				() -> ((InitializingBean) new SynchronizedItemStreamReader<>()).afterPropertiesSet());
+		assertEquals("A delegate item reader is required", expectedException.getMessage());
 	}
 
 }
--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/support/builder/SynchronizedItemStreamReaderBuilderTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/support/builder/SynchronizedItemStreamReaderBuilderTests.java
@@ -16,135 +16,36 @@
 
 package org.springframework.batch.item.support.builder;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.junit.jupiter.api.Test;
 
-import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.ItemStreamReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.support.AbstractItemStreamItemReader;
+import org.springframework.batch.item.support.AbstractSynchronizedItemStreamReaderTests;
 import org.springframework.batch.item.support.SynchronizedItemStreamReader;
-import org.springframework.lang.Nullable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Glenn Renfro
  * @author Mahmoud Ben Hassine
+ * @author Dimitrios Liapis
  */
-class SynchronizedItemStreamReaderBuilderTests {
+public class SynchronizedItemStreamReaderBuilderTests extends AbstractSynchronizedItemStreamReaderTests {
 
-	@Test
-	void testMultipleThreads() throws Exception {
-
-		// Initialized an ExecutionContext and a SynchronizedItemStreamReader to test.
-		final ExecutionContext executionContext = new ExecutionContext();
-
-		final SynchronizedItemStreamReaderBuilderTests.TestItemReader testItemReader = new SynchronizedItemStreamReaderBuilderTests.TestItemReader();
-		final SynchronizedItemStreamReader<Integer> synchronizedItemStreamReader = new SynchronizedItemStreamReaderBuilder<Integer>()
-			.delegate(testItemReader)
-			.build();
-
-		// Open the ItemReader and make sure it's initialized properly.
-		synchronizedItemStreamReader.open(executionContext);
-		assertEquals(true,
-				executionContext.get(SynchronizedItemStreamReaderBuilderTests.TestItemReader.HAS_BEEN_OPENED));
-		assertFalse(testItemReader.isClosed());
-
-		/*
-		 * Set up SIZE threads that read from the reader and updates the execution
-		 * context.
-		 */
-		final Set<Integer> ecSet = new HashSet<>();
-		final int SIZE = 20;
-		Thread[] threads = new Thread[SIZE];
-		for (int i = 0; i < SIZE; i++) {
-			threads[i] = new Thread() {
-				public void run() {
-					try {
-						ecSet.add(synchronizedItemStreamReader.read());
-						synchronizedItemStreamReader.update(executionContext);
-					}
-					catch (Exception ignore) {
-					}
-				}
-			};
-		}
-
-		// Start the threads and block until all threads are done.
-		for (Thread thread : threads) {
-			thread.run();
-		}
-		for (Thread thread : threads) {
-			thread.join();
-		}
-		testItemReader.close();
-
-		/*
-		 * Ensure cleanup happens as expected: status variable is set correctly and
-		 * ExecutionContext variable is set properly. Lastly, the Set<Integer> should have
-		 * 1 to 20 which may not always be the case if the read is not synchronized.
-		 */
-		for (int i = 1; i <= SIZE; i++) {
-			assertTrue(ecSet.contains(i));
-		}
-		assertTrue(testItemReader.isClosed());
-		assertEquals(SIZE,
-				executionContext.getInt(SynchronizedItemStreamReaderBuilderTests.TestItemReader.UPDATE_COUNT_KEY));
+	@Override
+	protected SynchronizedItemStreamReader<Object> createNewSynchronizedItemStreamReader() {
+		return new SynchronizedItemStreamReaderBuilder<>().delegate(delegate).build();
 	}
 
-	/**
-	 * A simple class used to test the SynchronizedItemStreamReader. It simply returns the
-	 * number of times the read method has been called, manages some state variables and
-	 * updates an ExecutionContext.
-	 *
-	 * @author Matthew Ouyang
-	 *
-	 */
-	private class TestItemReader extends AbstractItemStreamItemReader<Integer> implements ItemStreamReader<Integer> {
+	@Test
+	void testBuilderDelegateIsNotNull() {
+		// given
+		final SynchronizedItemStreamReaderBuilder<Object> builder = new SynchronizedItemStreamReaderBuilder<>();
 
-		private int cursor = 0;
+		// when
+		final Exception expectedException = assertThrows(IllegalArgumentException.class, builder::build);
 
-		private boolean isClosed = false;
-
-		public static final String HAS_BEEN_OPENED = "hasBeenOpened";
-
-		public static final String UPDATE_COUNT_KEY = "updateCount";
-
-		@Nullable
-		public Integer read() throws Exception, ParseException, NonTransientResourceException {
-			cursor = cursor + 1;
-			return cursor;
-		}
-
-		public void close() {
-			this.isClosed = true;
-		}
-
-		public void open(ExecutionContext executionContext) {
-			this.isClosed = false;
-			executionContext.put(HAS_BEEN_OPENED, true);
-			executionContext.remove(UPDATE_COUNT_KEY);
-		}
-
-		public void update(ExecutionContext executionContext) {
-
-			if (!executionContext.containsKey(UPDATE_COUNT_KEY)) {
-				executionContext.putInt(UPDATE_COUNT_KEY, 0);
-			}
-
-			executionContext.putInt(UPDATE_COUNT_KEY, executionContext.getInt(UPDATE_COUNT_KEY) + 1);
-		}
-
-		public boolean isClosed() {
-			return this.isClosed;
-		}
-
+		// then
+		assertEquals("A delegate is required", expectedException.getMessage());
 	}
 
 }
