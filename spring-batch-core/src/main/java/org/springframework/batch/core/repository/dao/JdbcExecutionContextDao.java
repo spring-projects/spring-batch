50d141d0d3c1256c997e390d9f510eadc5cc91e4
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2018-12-11 19:16:16 +0100

    Add assertion that a serializer was set in the JdbcExecutionContextDao
    Resolves BATCH-2779

--- spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/JdbcExecutionContextDao.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/JdbcExecutionContextDao.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2013 the original author or authors.
+ * Copyright 2006-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,7 @@ import org.springframework.util.Assert;
  * @author Thomas Risberg
  * @author Michael Minella
  * @author David Turanski
+ * @author Mahmoud Ben Hassine
  */
 public class JdbcExecutionContextDao extends AbstractJdbcBatchMetadataDao implements ExecutionContextDao {
 
@@ -87,6 +88,7 @@ public class JdbcExecutionContextDao extends AbstractJdbcBatchMetadataDao implem
 	 * @param serializer {@link ExecutionContextSerializer} instance to use.
 	 */
 	public void setSerializer(ExecutionContextSerializer serializer) {
+		Assert.notNull(serializer, "Serializer must not be null");
 		this.serializer = serializer;
 	}
 
@@ -208,6 +210,7 @@ public class JdbcExecutionContextDao extends AbstractJdbcBatchMetadataDao implem
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		super.afterPropertiesSet();
+		Assert.state(serializer != null, "ExecutionContextSerializer is required");
 	}
 
 	/**
