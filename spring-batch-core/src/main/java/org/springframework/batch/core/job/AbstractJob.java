2eb77c29bae1d637a8ab38c98c41f83984e67284
Saikat Bhadra <saikat.bhadra@gmail.com>
2019-01-29 06:16:23 +0100

    Fix typo in log message
    Resolves BATCH-2782
    
    (cherry picked from commit 30365edf589436ef5f7978bd97979fb608a15624)

--- spring-batch-core/src/main/java/org/springframework/batch/core/job/AbstractJob.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/AbstractJob.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2014 the original author or authors.
+ * Copyright 2006-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -353,7 +353,7 @@ InitializingBean {
 				try {
 					listener.afterJob(execution);
 				} catch (Exception e) {
-					logger.error("Exception encountered in afterStep callback", e);
+					logger.error("Exception encountered in afterJob callback", e);
 				}
 
 				jobRepository.update(execution);
