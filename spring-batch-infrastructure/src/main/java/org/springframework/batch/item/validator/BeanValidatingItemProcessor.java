b4978231b584ef9bffcdb161c6da8b39df5ca7e0
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2019-04-02 09:58:19 +0200

    Fix import in BeanValidatingItemProcessor
    Resolves BATCH-2802

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/validator/BeanValidatingItemProcessor.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/validator/BeanValidatingItemProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,8 +18,7 @@ package org.springframework.batch.item.validator;
 
 import javax.validation.Validator;
 
-import org.apache.shiro.util.Assert;
-
+import org.springframework.util.Assert;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
