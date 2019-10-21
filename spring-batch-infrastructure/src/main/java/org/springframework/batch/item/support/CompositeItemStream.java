e7647b94c3cc237dd211edd0a69223a742f0fd3a
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2019-10-21 11:54:30 +0200

    Polish 43b31ae89c9ad613a49cc89ef10a9f8e5f3f0955

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/CompositeItemStream.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/CompositeItemStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 the original author or authors.
+ * Copyright 2006-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import org.springframework.batch.item.ItemStreamException;
  * Simple {@link ItemStream} that delegates to a list of other streams.
  * 
  * @author Dave Syer
+ * @author Mahmoud Ben Hassine
  * 
  */
 public class CompositeItemStream implements ItemStream {
