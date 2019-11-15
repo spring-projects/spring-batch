397f78f15d0b943a7788443cfa0d25a0b1a4e386
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2019-11-15 17:46:58 +0100

    Remove the requirement for a Resource to be set
    This commit removes the requirement for a Resource to be configured at
    build time. This requirement prevented the use of this builder along
    with the MultiResourceItemWriter.
    
    Resolves BATCH-2720

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemWriterBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemWriterBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 package org.springframework.batch.item.file.builder;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.batch.item.file.FlatFileFooterCallback;
 import org.springframework.batch.item.file.FlatFileHeaderCallback;
@@ -31,6 +34,8 @@ import org.springframework.util.Assert;
  * @see FlatFileItemWriter
  */
 public class FlatFileItemWriterBuilder<T> {
+
+	protected Log logger = LogFactory.getLog(getClass());
 
 	private Resource resource;
 
@@ -242,10 +247,14 @@ public class FlatFileItemWriterBuilder<T> {
 	public FlatFileItemWriter<T> build() {
 
 		Assert.notNull(this.lineAggregator, "A LineAggregator is required");
-		Assert.notNull(this.resource, "A Resource is required");
 
 		if(this.saveState) {
 			Assert.hasText(this.name, "A name is required when saveState is true");
+		}
+
+		if(this.resource == null) {
+			logger.debug("The resource is null. This is only a valid scenario when " +
+					"injecting it later as in when using the MultiResourceItemWriter");
 		}
 
 		FlatFileItemWriter<T> writer = new FlatFileItemWriter<>();
