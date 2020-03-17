1a6ecfdb5d7a86fc0c674dfd73d830e64a5aa53a
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2020-03-17 23:27:47 +0100

    Fix line tokenizer validation in FlatFileItemReaderBuilder
    Resolves #766

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemReaderBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/file/builder/FlatFileItemReaderBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 the original author or authors.
+ * Copyright 2016-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -474,7 +474,7 @@ public class FlatFileItemReaderBuilder<T> {
 
 			DefaultLineMapper<T> lineMapper = new DefaultLineMapper<>();
 
-			if(this.lineTokenizer != null && this.fieldSetMapper != null) {
+			if(this.lineTokenizer != null) {
 				lineMapper.setLineTokenizer(this.lineTokenizer);
 			}
 			else if(this.fixedLengthBuilder != null) {
