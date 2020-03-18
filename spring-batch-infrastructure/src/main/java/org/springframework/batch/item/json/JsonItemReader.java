b9fa9c37867829762e61e499c1d16a20b1c9e9c9
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2020-03-19 05:58:54 +0900

    Fix constructor of JsonItemReader to call setExecutionContextName()
    Resolves #3681

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/JsonItemReader.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/JsonItemReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the original author or authors.
+ * Copyright 2018-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,7 @@ import org.springframework.batch.item.support.AbstractItemCountingItemStreamItem
 import org.springframework.core.io.Resource;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 /**
  * {@link ItemStreamReader} implementation that reads Json objects from a
@@ -70,6 +71,7 @@ public class JsonItemReader<T> extends AbstractItemCountingItemStreamItemReader<
 		Assert.notNull(jsonObjectReader, "The json object reader must not be null.");
 		this.resource = resource;
 		this.jsonObjectReader = jsonObjectReader;
+		setExecutionContextName(ClassUtils.getShortName(JsonItemReader.class));
 	}
 
 	/**
--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/JsonItemReaderTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/JsonItemReaderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -104,6 +104,7 @@ public class JsonItemReaderTests {
 		itemReader = new JsonItemReader<>(resource, this.jsonObjectReader);
 
 		// when
+		itemReader.open(new ExecutionContext());
 		itemReader.read();
 
 		// then
