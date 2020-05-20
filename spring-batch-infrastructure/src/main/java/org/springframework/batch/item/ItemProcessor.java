91aa17b22ff7dbca683f1af3ef66e63562e8267d
Mahmoud Ben Hassine <mbenhassine@pivotal.io>
2020-05-20 13:25:51 +0200

    Polish f37fae670b454699092abd0494a29706af6a271f
    * Update year in license header
    * Update Javadoc formatting

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/ItemProcessor.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/ItemProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2018 the original author or authors.
+ * Copyright 2006-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,28 +20,35 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 /**
- * Interface for item transformation.  Given an item as input, this interface provides
+ * Interface for item transformation. Given an item as input, this interface provides
  * an extension point which allows for the application of business logic in an item 
- * oriented processing scenario.  It should be noted that while it's possible to return
- * a different type than the one provided, it's not strictly necessary.  Furthermore, 
- * returning null indicates that the item should not be continued to be processed.
+ * oriented processing scenario. It should be noted that while it's possible to return
+ * a different type than the one provided, it's not strictly necessary. Furthermore, 
+ * returning {@code null} indicates that the item should not be continued to be processed.
  *  
  * @author Robert Kasanicky
  * @author Dave Syer
  * @author Mahmoud Ben Hassine
+ * 
+ * @param <I> type of input item
+ * @param <O> type of output item
  */
 public interface ItemProcessor<I, O> {
 
 	/**
 	 * Process the provided item, returning a potentially modified or new item for continued
-	 * processing.  If the returned result is null, it is assumed that processing of the item
+	 * processing.  If the returned result is {@code null}, it is assumed that processing of the item
 	 * should not continue.
 	 * 
-	 * @param item to be processed.  A {@code null} will never reach this method because the only possible sources 
-	 * are ItemReader (which indicates no more items) and ItemProcessor (which indicates a filtered item). 
+	 * A {@code null} item will never reach this method because the only possible sources are:
+	 * <ul>
+	 *     <li>an {@link ItemReader} (which indicates no more items)</li>
+	 *     <li>a previous {@link ItemProcessor} in a composite processor (which indicates a filtered item)</li>
+	 * </ul>
+	 * 
+	 * @param item to be processed, never {@code null}.
 	 * @return potentially modified or new item for continued processing, {@code null} if processing of the
 	 *  provided item should not continue.
-	 *
 	 * @throws Exception thrown if exception occurs during processing.
 	 */
 	@Nullable
