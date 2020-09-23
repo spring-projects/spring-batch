8e821fe1c5177f0d4b27c9d658be3825f22ae07a
Parikshit Dutta <parikshitdutta@hotmail.com>
2020-09-23 20:14:26 +0530

    Fix for setting name of StaxEventItemReader at build

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/xml/builder/StaxEventItemReaderBuilderTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/xml/builder/StaxEventItemReaderBuilderTests.java
@@ -34,6 +34,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
+import static org.springframework.test.util.ReflectionTestUtils.getField;
 
 /**
  * @author Michael Minella
@@ -119,6 +120,9 @@ public class StaxEventItemReaderBuilderTests {
 		assertEquals("five", item.getSecond());
 		assertEquals("six", item.getThird());
 		assertEquals(2, executionContext.size());
+
+		Object executionContextUserSupport = getField(reader, "executionContextUserSupport");
+		assertEquals("fooReader", getField(executionContextUserSupport, "name"));
 	}
 
 	@Test(expected = ItemStreamException.class)
