f2c1296e7f52050c0c1dc29870849d42f6faa263
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2020-11-09 11:54:22 +0100

    Fix ClassCastException in RunIdIncrementer
    Before this commit, the RunIdIncrementer was failing
    with a ClassCastException if the run.id parameter is
    not passed as a Long.
    
    This commit makes the RunIdIncrementer more liberal
    in what it accepts by trying to parse the parameter
    to a Long.
    
    Resolves #3799

--- spring-batch-core/src/main/java/org/springframework/batch/core/launch/support/RunIdIncrementer.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/launch/support/RunIdIncrementer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2018 the original author or authors.
+ * Copyright 2006-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,18 @@
  */
 package org.springframework.batch.core.launch.support;
 
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersIncrementer;
 import org.springframework.lang.Nullable;
 
 /**
+ * This incrementer increments a "run.id" parameter of type {@link Long}
+ * from the given job parameters. If the parameter does not exist, it will
+ * be initialized to 1. The parameter name can be configured using
+ * {@link #setKey(String)}.
+ * 
  * @author Dave Syer
  * @author Mahmoud Ben Hassine
  */
@@ -41,14 +47,27 @@ public class RunIdIncrementer implements JobParametersIncrementer {
 
 	/**
 	 * Increment the run.id parameter (starting with 1).
+	 *
+	 * @param parameters the previous job parameters
+	 * @return the next job parameters with an incremented (or initialized) run.id
+	 * @throws IllegalArgumentException if the previous value of run.id is invalid
 	 */
 	@Override
 	public JobParameters getNext(@Nullable JobParameters parameters) {
 
 		JobParameters params = (parameters == null) ? new JobParameters() : parameters;
-
-		long id = params.getLong(key, new Long(0)) + 1;
-		return new JobParametersBuilder(params).addLong(key, id).toJobParameters();
+		JobParameter runIdParameter = params.getParameters().get(this.key);
+		long id = 1;
+		if (runIdParameter != null) {
+			try {
+				id = Long.parseLong(runIdParameter.getValue().toString()) + 1;
+			}
+			catch (NumberFormatException exception) {
+				throw new IllegalArgumentException("Invalid value for parameter "
+						+ this.key, exception);
+			}
+		}
+		return new JobParametersBuilder(params).addLong(this.key, id).toJobParameters();
 	}
 
 }
