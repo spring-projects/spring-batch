5c1a6083fd432bdfaeb69303ba80785c63695f2b
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2020-11-27 23:17:56 +0100

    Polish contribution feb46fd3f74716db42e2d34639d4a6952e7267e5
    * Update year in license header
    * Use "this" keyword where appropriate

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/kafka/KafkaItemWriterTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/kafka/KafkaItemWriterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors.
+ * Copyright 2019-2020 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -111,9 +111,9 @@ public class KafkaItemWriterTests {
 				this.kafkaTemplate.sendDefault(key, value);
 			}
 		};
-		kafkaItemWriter.setKafkaTemplate(kafkaTemplate);
+		kafkaItemWriter.setKafkaTemplate(this.kafkaTemplate);
 		kafkaItemWriter.writeKeyValue("k", "v");
-		verify(kafkaTemplate).sendDefault("k", "v");
+		verify(this.kafkaTemplate).sendDefault("k", "v");
 	}
 
 	static class KafkaItemKeyMapper implements Converter<String, String> {
