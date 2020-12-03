1b936a28090570b17f9c53f3927d0aa7ee56add9
s.h.bae <shbae0121@gmail.com>
2020-12-03 18:35:00 +0100

    Fix javadoc on ItemReadListener interface
    Found after reading some javadocs.
    This seems to be a copy & paste mistake.
    
    (cherry picked from commit f51ed50141db34a6d4ea6074e576fb1d57d7647f)

--- spring-batch-core/src/main/java/org/springframework/batch/core/ItemReadListener.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/ItemReadListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2018 the original author or authors.
+ * Copyright 2006-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ public interface ItemReadListener<T> extends StepListener {
 	/**
 	 * Called if an error occurs while trying to read.
 	 * 
-	 * @param ex thrown from {@link ItemWriter}
+	 * @param ex thrown from {@link ItemReader}
 	 */
 	void onReadError(Exception ex);
 }
