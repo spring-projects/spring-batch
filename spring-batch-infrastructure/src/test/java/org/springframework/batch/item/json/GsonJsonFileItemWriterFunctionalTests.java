2cc807de6237d48cd6b6f14f7ed9a29a4530ab17
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2021-02-04 16:01:22 +0100

    Fix tests failing on Windows
    Before this commit, tests in this change set
    were failing on MS Windows due to the file
    comparison method which was based on file
    content hash comparison. With this method,
    differences between OS line endings
    (LF vs CRLF) in generated json files produce
    different hashes.
    
    This commit uses a logical json comparison
    based on the jsonassert library.

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/GsonJsonFileItemWriterFunctionalTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/GsonJsonFileItemWriterFunctionalTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,11 @@ public class GsonJsonFileItemWriterFunctionalTests extends JsonFileItemWriterFun
 	@Override
 	protected String getExpectedPrettyPrintedFile() {
 		return "expected-trades-gson-pretty-print.json";
+	}
+
+	@Override
+	protected String getMarshallerName() {
+		return "gson";
 	}
 
 }
--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/JacksonJsonFileItemWriterFunctionalTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/json/JacksonJsonFileItemWriterFunctionalTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,6 +43,11 @@ public class JacksonJsonFileItemWriterFunctionalTests extends JsonFileItemWriter
 	@Override
 	protected String getExpectedPrettyPrintedFile() {
 		return "expected-trades-jackson-pretty-print.json";
+	}
+
+	@Override
+	protected String getMarshallerName() {
+		return "jackson";
 	}
 
 }
