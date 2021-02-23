7b2d7d1993b18c3f819bfa49684356072ab5fdc3
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2021-02-23 15:56:51 +0100

    Refine af14a2e4718f7c51411df19a562738a4d136f200
    * Make default constructors call other constructors with default values
    * Fix formatting
    * Update year in license headers

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/JacksonJsonObjectMarshaller.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/JacksonJsonObjectMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,11 +34,11 @@ public class JacksonJsonObjectMarshaller<T> implements JsonObjectMarshaller<T> {
 	private ObjectMapper objectMapper;
 
 	public JacksonJsonObjectMarshaller() {
-		this.objectMapper = new ObjectMapper();
+		this(new ObjectMapper());
 	}
 
 	public JacksonJsonObjectMarshaller(ObjectMapper objectMapper) {
-		this.objectMapper=objectMapper;
+		this.objectMapper = objectMapper;
 	}
 
 	/**
