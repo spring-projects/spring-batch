85e71d1e0724f3ec7d6510df220f346cab94a9fa
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2021-02-23 15:56:51 +0100

    Refine af14a2e4718f7c51411df19a562738a4d136f200
    * Make default constructors call other constructors with default values
    * Fix formatting
    * Update year in license headers

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/GsonJsonObjectMarshaller.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/GsonJsonObjectMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,14 +28,14 @@ import com.google.gson.Gson;
  */
 public class GsonJsonObjectMarshaller<T> implements JsonObjectMarshaller<T> {
 
-	private Gson gson = new Gson();
+	private Gson gson;
 
 	public GsonJsonObjectMarshaller() {
-		this.gson=new Gson();
+		this(new Gson());
 	}
 	
 	public GsonJsonObjectMarshaller(Gson gson) {
-		this.gson=gson;
+		this.gson = gson;
 	}
 	
 	/**
--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/GsonJsonObjectReader.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/json/GsonJsonObjectReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 the original author or authors.
+ * Copyright 2018-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,8 +55,7 @@ public class GsonJsonObjectReader<T> implements JsonObjectReader<T> {
 	 * @param itemType the target item type
 	 */
 	public GsonJsonObjectReader(Class<? extends T> itemType) {
-		this.mapper = new Gson();
-		this.itemType = itemType;
+		this(new Gson(), itemType);
 	}
 
 	public GsonJsonObjectReader(Gson mapper, Class<? extends T> itemType) {
