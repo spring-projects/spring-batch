ff2bcdba07af8ac90a0cc540753e09f5e43cfd60
Parikshit Dutta <parikshitdutta@hotmail.com>
2021-04-23 21:20:26 +0530

    Updated verifyCursorPosition default to true in JdbcCursorItemReaderBuilder
    Issue #3893

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/builder/JdbcCursorItemReaderBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/builder/JdbcCursorItemReaderBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 the original author or authors.
+ * Copyright 2016-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ import org.springframework.util.StringUtils;
  * @author Drummond Dawson
  * @author Mahmoud Ben Hassine
  * @author Ankur Trapasiya
+ * @author Parikshit Dutta
  * @since 4.0
  */
 public class JdbcCursorItemReaderBuilder<T> {
@@ -50,7 +51,7 @@ public class JdbcCursorItemReaderBuilder<T> {
 
 	private boolean ignoreWarnings;
 
-	private boolean verifyCursorPosition;
+	private boolean verifyCursorPosition = true;
 
 	private boolean driverSupportsAbsolute;
 
