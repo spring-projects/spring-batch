7a3d26c9f78fecf3f86675501aba6b062a6551d2
Gregor Riegler <rieglerg85@gmail.com>
2021-06-17 12:42:00 +0000

    simplify boolean expression
    Co-authored-by: Moderne <team@moderne.io>

--- spring-batch-core/src/main/java/org/springframework/batch/core/configuration/xml/StepParserStepFactoryBean.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/configuration/xml/StepParserStepFactoryBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2013 the original author or authors.
+ * Copyright 2006-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -361,7 +361,7 @@ public class StepParserStepFactoryBean<I, O> implements FactoryBean<Step>, BeanN
 			builder.processorNonTransactional();
 		}
 
-		if (readerTransactionalQueue!=null && readerTransactionalQueue==true) {
+		if (readerTransactionalQueue != null && readerTransactionalQueue) {
 			builder.readerIsTransactionalQueue();
 		}
 
