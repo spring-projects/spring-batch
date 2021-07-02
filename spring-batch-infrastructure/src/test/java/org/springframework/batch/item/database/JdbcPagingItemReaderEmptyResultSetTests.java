78ddf44730e50548f77ae72239527b5720766010
Henning Pöttker <Henning.Poettker@c24.de>
2021-07-03 00:37:25 +0200

    Fix multi-threaded empty read for JdbcPagingItemReader
    On empty input, the JdbcPagingItemReader cannot derive
    a start value for the sort key to be used in further
    queries. For multi-threaded steps, it is thus necessary
    to prevent the reader from trying to read further pages
    if the first page is empty.
    
    Issue #3898

---
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/database/JdbcPagingItemReaderEmptyResultSetTests.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.item.database;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.Collections;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.support.HsqlPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "JdbcPagingItemReaderCommonTests-context.xml")
+public class JdbcPagingItemReaderEmptyResultSetTests {
+
+	private static final int PAGE_SIZE = 2;
+	private static final int EMPTY_READS = PAGE_SIZE + 1;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Test
+	public void testMultiplePageReadsOnEmptyResultSet() throws Exception {
+		final ItemReader<Long> reader = getItemReader();
+		for (int i = 0; i < EMPTY_READS; i++) {
+			assertNull(reader.read());
+		}
+	}
+
+	private ItemReader<Long> getItemReader() throws Exception {
+		JdbcPagingItemReader<Long> reader = new JdbcPagingItemReader<>();
+		reader.setDataSource(dataSource);
+		HsqlPagingQueryProvider queryProvider = new HsqlPagingQueryProvider();
+		queryProvider.setSelectClause("select ID");
+		queryProvider.setFromClause("from T_FOOS");
+		queryProvider.setWhereClause("1 = 0");
+		queryProvider.setSortKeys(Collections.singletonMap("ID", Order.ASCENDING));
+		reader.setQueryProvider(queryProvider);
+		reader.setRowMapper(new SingleColumnRowMapper<>());
+		reader.setPageSize(PAGE_SIZE);
+		reader.afterPropertiesSet();
+		reader.setSaveState(false);
+
+		return reader;
+	}
+}
