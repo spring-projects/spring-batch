03d56f6834a18b8d0c268625760e5a03b131a6c0
sjh836 <sjh836@gmail.com>
2021-09-15 01:50:39 +0900

    Assign a static collection to a local variable in StepContext#getPartitionPlan

--- spring-batch-core/src/main/java/org/springframework/batch/core/scope/context/StepContext.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/scope/context/StepContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2019 the original author or authors.
+ * Copyright 2006-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,7 +160,7 @@ public class StepContext extends SynchronizedAttributeAccessor {
 
 	@SuppressWarnings({"rawtypes", "unchecked"})
 	public Map<String, Object> getPartitionPlan() {
-		Map<String, Object> partitionPlanProperties = new HashMap<>();
+		Map<String, Object> partitionPlanProperties = Collections.emptyMap();
 
 		if(propertyContext != null) {
 			Map partitionProperties = propertyContext.getStepProperties(getStepName());
--- spring-batch-core/src/test/java/org/springframework/batch/core/scope/context/StepContextTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/scope/context/StepContextTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2019 the original author or authors.
+ * Copyright 2006-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,6 +79,14 @@ public class StepContextTests {
 
 		Map<String, Object> plan = context.getPartitionPlan();
 		assertEquals("value1", plan.get("key1"));
+	}
+
+	@Test
+	public void testGetPartitionPlanWhenPropertyContextIsNull() {
+		context = new StepContext(stepExecution, null);
+
+		Map<String, Object> plan = context.getPartitionPlan();
+		assertEquals(0, plan.size());
 	}
 
 	@Test
