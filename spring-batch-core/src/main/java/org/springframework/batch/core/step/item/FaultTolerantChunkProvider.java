73bfaf3de41631b7d587216f88e1d0f354f71771
Aaron <89061309+aaronsleepy@users.noreply.github.com>
2021-11-29 11:08:09 +0100

    Fix javadoc of FaultTolerantChunkProvider
    FaultTolerantChunkProvider is an implementation
    of ChunkProvider, not ChunkProcessor.
    
    Issue #4029

--- spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProvider.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 the original author or authors.
+ * Copyright 2006-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import org.springframework.classify.BinaryExceptionClassifier;
 import org.springframework.classify.Classifier;
 
 /**
- * FaultTolerant implementation of the {@link ChunkProcessor} interface, that
+ * FaultTolerant implementation of the {@link ChunkProvider} interface, that
  * allows for skipping or retry of items that cause exceptions during reading or
  * processing.
  * 
