db2d622145c0c9dd19fca7c2e323e8d9fa7a4829
Henning Poettker <henning.poettker@c24.de>
2022-01-17 23:30:32 +0100

    Adapt H2PagingQueryProvider for H2 v2.x
    Issue #4047

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/H2PagingQueryProvider.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/H2PagingQueryProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2008 the original author or authors.
+ * Copyright 2006-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,22 +20,23 @@ package org.springframework.batch.item.database.support;
  * H2 implementation of a {@link org.springframework.batch.item.database.PagingQueryProvider} using database specific features.
  *
  * @author Dave Syer
+ * @author Henning Pöttker
  * @since 2.1
  */
 public class H2PagingQueryProvider extends AbstractSqlPagingQueryProvider {
 
 	@Override
 	public String generateFirstPageQuery(int pageSize) {
-		return SqlPagingQueryUtils.generateTopSqlQuery(this, false, buildTopClause(pageSize));
+		return SqlPagingQueryUtils.generateLimitSqlQuery(this, false, buildLimitClause(pageSize));
 	}
 
 	@Override
 	public String generateRemainingPagesQuery(int pageSize) {
-		return SqlPagingQueryUtils.generateTopSqlQuery(this, true, buildTopClause(pageSize));
+		return SqlPagingQueryUtils.generateLimitSqlQuery(this, true, buildLimitClause(pageSize));
 	}
 
-	private String buildTopClause(int pageSize) {
-		return new StringBuilder().append("TOP ").append(pageSize).toString();
+	private String buildLimitClause(int pageSize) {
+		return new StringBuilder().append("FETCH NEXT ").append(pageSize).append(" ROWS ONLY").toString();
 	}
 
 	@Override
@@ -44,8 +45,10 @@ public class H2PagingQueryProvider extends AbstractSqlPagingQueryProvider {
 		int offset = (page * pageSize) - 1;
 		offset = offset<0 ? 0 : offset;
 
-		String topClause = new StringBuilder().append("LIMIT ").append(offset).append(" 1").toString();
-		return SqlPagingQueryUtils.generateTopJumpToQuery(this, topClause);
+		String limitClause = new StringBuilder().append("OFFSET ")
+				.append(offset).append(" ROWS FETCH NEXT 1 ROWS ONLY")
+				.toString();
+		return SqlPagingQueryUtils.generateLimitJumpToQuery(this, limitClause);
 	}
 
 }
