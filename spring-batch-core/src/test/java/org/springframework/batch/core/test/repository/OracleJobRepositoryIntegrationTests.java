5c645c274709bc0a7bb9a484eb018e1fad4af845
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2022-03-22 17:29:59 +0100

    Rename schema-oracle10g.sql to schema-oracle.sql
    Resolves ##1057

--- spring-batch-core/src/test/java/org/springframework/batch/core/test/repository/OracleJobRepositoryIntegrationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/test/repository/OracleJobRepositoryIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,7 +73,7 @@ public class OracleJobRepositoryIntegrationTests {
 	@Before
 	public void setUp() {
 		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-		databasePopulator.addScript(new ClassPathResource("/org/springframework/batch/core/schema-oracle10g.sql"));
+		databasePopulator.addScript(new ClassPathResource("/org/springframework/batch/core/schema-oracle.sql"));
 		databasePopulator.execute(this.dataSource);
 	}
 
