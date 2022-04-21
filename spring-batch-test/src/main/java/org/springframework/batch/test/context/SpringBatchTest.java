ad92d2a6c9988ba6f126cdf347a4bbf47a09743f
Starchenko Ilya <redux1234567@mail.ru>
2022-04-21 18:47:51 +0600

    Fix Javadoc of SpringBatchTest annotation
    fix double @-symbol in javadoc
    
    Issue #4102

--- spring-batch-test/src/main/java/org/springframework/batch/test/context/SpringBatchTest.java
+++ spring-batch-test/src/main/java/org/springframework/batch/test/context/SpringBatchTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 the original author or authors.
+ * Copyright 2018-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,10 +56,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * &#064;ContextConfiguration(classes = MyBatchJobConfiguration.class)
  * public class MyBatchJobTests {
  *
- *    &#064;@Autowired
+ *    &#064;Autowired
  *    private JobLauncherTestUtils jobLauncherTestUtils;
  *
- *    &#064;@Autowired
+ *    &#064;Autowired
  *    private JobRepositoryTestUtils jobRepositoryTestUtils;
  *
  *    &#064;Before
@@ -91,10 +91,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
  * &#064;ContextConfiguration(classes = MyBatchJobConfiguration.class)
  * public class MyBatchJobTests {
  *
- *    &#064;@Autowired
+ *    &#064;Autowired
  *    private JobLauncherTestUtils jobLauncherTestUtils;
  *
- *    &#064;@Autowired
+ *    &#064;Autowired
  *    private JobRepositoryTestUtils jobRepositoryTestUtils;
  *
  *    &#064;BeforeEach
