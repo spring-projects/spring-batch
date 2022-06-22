3454cec27904ccbbeb551e4408f74e690e677081
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2022-06-22 10:58:57 +0200

    Fix annotation based listener registration in FaultTolerantStepBuilder
    Resolves #4137

--- spring-batch-core/src/main/java/org/springframework/batch/core/step/builder/FaultTolerantStepBuilder.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/step/builder/FaultTolerantStepBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2021 the original author or authors.
+ * Copyright 2006-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -190,8 +190,7 @@ public class FaultTolerantStepBuilder<I, O> extends SimpleStepBuilder<I, O> {
 	 * @return this for fluent chaining
 	 */
 	@Override
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public SimpleStepBuilder<I, O> listener(Object listener) {
+	public FaultTolerantStepBuilder<I, O> listener(Object listener) {
 		super.listener(listener);
 
 		Set<Method> skipListenerMethods = new HashSet<>();
@@ -205,9 +204,7 @@ public class FaultTolerantStepBuilder<I, O> extends SimpleStepBuilder<I, O> {
 			skipListeners.add((SkipListener) factory.getObject());
 		}
 
-		@SuppressWarnings("unchecked")
-		SimpleStepBuilder<I, O> result = this;
-		return result;
+		return this;
 	}
 
 	/**
--- spring-batch-core/src/test/java/org/springframework/batch/core/step/builder/FaultTolerantStepBuilderTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/step/builder/FaultTolerantStepBuilderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2021-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,9 +15,15 @@
  */
 package org.springframework.batch.core.step.builder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.xml.DummyItemReader;
+import org.springframework.batch.core.configuration.xml.DummyItemWriter;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class FaultTolerantStepBuilderTests {
 
@@ -25,6 +31,20 @@ public class FaultTolerantStepBuilderTests {
 	public void faultTolerantReturnsSameInstance() {
 		FaultTolerantStepBuilder<Object, Object> builder = new FaultTolerantStepBuilder<>(new StepBuilder("test"));
 		assertEquals(builder, builder.faultTolerant());
+	}
+
+	@Test
+	public void testAnnotationBasedStepExecutionListenerRegistration() {
+		// given
+		FaultTolerantStepBuilder<Object, Object> faultTolerantStepBuilder = new StepBuilder("myStep")
+				.<Object, Object>chunk(5).reader(new DummyItemReader()).writer(new DummyItemWriter()).faultTolerant()
+				.listener(new StepBuilderTests.AnnotationBasedStepExecutionListener());
+
+		// when
+		Step step = faultTolerantStepBuilder.build();
+
+		// then
+		Assert.assertNotNull(step);
 	}
 
 }
