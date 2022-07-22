fc3404d6df0e7343002b2ccda057e3c071911912
Henning Poettker <Henning.Poettker@c24.de>
2022-07-22 22:37:31 +0200

    Replace ListenableFuture with CompletableFuture

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/kafka/KafkaItemWriterTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/kafka/KafkaItemWriterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package org.springframework.batch.item.kafka;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
@@ -28,7 +29,6 @@ import org.mockito.junit.MockitoRule;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -46,7 +46,7 @@ public class KafkaItemWriterTests {
 	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@Mock
-	private ListenableFuture<SendResult<String, String>> future;
+	private CompletableFuture<SendResult<String, String>> future;
 
 	private KafkaItemKeyMapper itemKeyMapper;
 
