8c7fb5ca02a1c46d27b27b09a0d724b30c25c7a7
Philippe Marschall <philippe.marschall@gmail.com>
2022-10-03 22:38:32 +0200

    Check the return value of File.delete()
    Check the return value of File.delete() and
    throw an exception if deleting a file fails.
    
    Issue #4203

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/AbstractFileItemWriter.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/AbstractFileItemWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2022 the original author or authors.
+ * Copyright 2006-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -270,7 +270,9 @@ public abstract class AbstractFileItemWriter<T> extends AbstractItemStreamItemWr
 				state.close();
 				if (state.linesWritten == 0 && shouldDeleteIfEmpty) {
 					try {
-						resource.getFile().delete();
+						if (!resource.getFile().delete()) {
+							throw new ItemStreamException("Failed to delete empty file on close");
+						}
 					}
 					catch (IOException e) {
 						throw new ItemStreamException("Failed to delete empty file on close", e);
