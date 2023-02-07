97e98e21b026a1baa11f53b2a203890145e69c13
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-02-07 22:37:48 +0100

    Add native proxy hint for JobOperator
    Resolves #4248

--- spring-batch-core/src/main/java/org/springframework/batch/core/aot/CoreRuntimeHints.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/aot/CoreRuntimeHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2022 the original author or authors.
+ * Copyright 2022-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,6 +79,9 @@ public class CoreRuntimeHints implements RuntimeHintsRegistrar {
 						.proxiedInterfaces(SpringProxy.class, Advised.class, DecoratingProxy.class))
 				.registerJdkProxy(builder -> builder
 						.proxiedInterfaces(TypeReference.of("org.springframework.batch.core.explore.JobExplorer"))
+						.proxiedInterfaces(SpringProxy.class, Advised.class, DecoratingProxy.class))
+				.registerJdkProxy(builder -> builder
+						.proxiedInterfaces(TypeReference.of("org.springframework.batch.core.launch.JobOperator"))
 						.proxiedInterfaces(SpringProxy.class, Advised.class, DecoratingProxy.class));
 
 		// reflection hints
