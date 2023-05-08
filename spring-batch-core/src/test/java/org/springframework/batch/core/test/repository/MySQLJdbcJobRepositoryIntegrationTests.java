01deb1f192c5fb688328947669bd62b92eca7e9d
Sukanth Gunda <contact.sukanth@gmail.com>
2023-05-08 07:57:27 -0500

    Updated license header name and added name to javadocs
    Updated license header name and added name to javadocs

--- spring-batch-core/src/test/java/org/springframework/batch/core/test/repository/MySQLJdbcJobRepositoryIntegrationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/test/repository/MySQLJdbcJobRepositoryIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022 the original author or authors.
+ * Copyright 2020-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,8 +60,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Mahmoud Ben Hassine
+ * @author Sukanth Gunda
  */
-@TestContainers(disabledWithoutDocker = true)
+@Testcontainers(disabledWithoutDocker = true)
 @SpringJUnitConfig
 class MySQLJdbcJobRepositoryIntegrationTests {
 
