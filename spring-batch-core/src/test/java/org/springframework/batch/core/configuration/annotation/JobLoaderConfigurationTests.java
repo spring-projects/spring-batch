fd8f65b76176c400afc986812bb54559c40dbf82
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-06-21 17:47:01 +0200

    Remove unused bean in JobLoaderConfigurationTests

--- spring-batch-core/src/test/java/org/springframework/batch/core/configuration/annotation/JobLoaderConfigurationTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/configuration/annotation/JobLoaderConfigurationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2022 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -119,12 +119,6 @@ class JobLoaderConfigurationTests {
 
 	@Configuration
 	public static class TestConfiguration {
-
-		@Bean
-		public ApplicationObjectSupport fakeApplicationObjectSupport() {
-			return new ApplicationObjectSupport() {
-			};
-		}
 
 		@Bean
 		public Job testJob(JobRepository jobRepository) throws Exception {
