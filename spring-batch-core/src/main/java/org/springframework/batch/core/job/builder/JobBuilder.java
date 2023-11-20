8ad6d806a54565b8afa5a403ea4e9b13d5fa3db0
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-11-20 11:53:49 +0100

    Fix JavaDoc in JobBuilder
    Issue #4415

--- spring-batch-core/src/main/java/org/springframework/batch/core/job/builder/JobBuilder.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/builder/JobBuilder.java
@@ -62,7 +62,7 @@ public class JobBuilder extends JobBuilderHelper<JobBuilder> {
 	/**
 	 * Create a new job builder that will execute a flow.
 	 * @param flow a flow to execute
-	 * @return a {@link SimpleJobBuilder}
+	 * @return a {@link JobFlowBuilder}
 	 */
 	public JobFlowBuilder start(Flow flow) {
 		return new FlowJobBuilder(this).start(flow);
@@ -71,7 +71,7 @@ public class JobBuilder extends JobBuilderHelper<JobBuilder> {
 	/**
 	 * Create a new job builder that will execute a step or sequence of steps.
 	 * @param step a step to execute
-	 * @return a {@link SimpleJobBuilder}
+	 * @return a {@link JobFlowBuilder}
 	 */
 	public JobFlowBuilder flow(Step step) {
 		return new FlowJobBuilder(this).start(step);
