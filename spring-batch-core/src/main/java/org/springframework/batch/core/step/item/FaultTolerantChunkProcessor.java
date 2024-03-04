a4a8ab7aaa8e3ead006157c08d845acfe9993e85
Mustafa Yanar <mustafayanar0007@gmail.com>
2024-03-04 22:07:58 +0300

    Fix output chunk end property in ChunkProcessor implementations
    Resolves #4560

--- spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProcessor.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2023 the original author or authors.
+ * Copyright 2006-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -306,7 +306,9 @@ public class FaultTolerantChunkProcessor<I, O> extends SimpleChunkProcessor<I, O
 				break;
 			}
 		}
-
+		if (inputs.isEnd()) {
+			outputs.setEnd();
+		}
 		return outputs;
 
 	}
