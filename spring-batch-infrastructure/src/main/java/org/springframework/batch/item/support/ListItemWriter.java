605757ac21a58604e136db04c3264344bae15fe9
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2024-05-03 08:55:01 +0100

    Remove wildcard return type in ListItemWriter
    Resolves #4576
    
    Signed-off-by: Fabrice Bibonne <fabrice.bibonne@gmail.com>

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/ListItemWriter.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/ListItemWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2023 the original author or authors.
+ * Copyright 2014-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ public class ListItemWriter<T> implements ItemWriter<T> {
 		writtenItems.addAll(chunk.getItems());
 	}
 
-	public List<? extends T> getWrittenItems() {
+	public List<T> getWrittenItems() {
 		return this.writtenItems;
 	}
 
