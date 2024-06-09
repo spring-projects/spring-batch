68349bbde2a0ab010e5ab30766bed681a2a109b8
Taeik Lim <sibera21@gmail.com>
2024-06-09 22:22:35 +0900

    Add '@FunctionalInterface' to JobKeyGenerator
    Signed-off-by: Taeik Lim <sibera21@gmail.com>

--- spring-batch-core/src/main/java/org/springframework/batch/core/JobKeyGenerator.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/JobKeyGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2022 the original author or authors.
+ * Copyright 2013-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,9 +21,11 @@ package org.springframework.batch.core;
  *
  * @author Michael Minella
  * @author Mahmoud Ben Hassine
+ * @author Taeik Lim
  * @param <T> The type of the source data used to calculate the key.
  * @since 2.2
  */
+@FunctionalInterface
 public interface JobKeyGenerator<T> {
 
 	/**
