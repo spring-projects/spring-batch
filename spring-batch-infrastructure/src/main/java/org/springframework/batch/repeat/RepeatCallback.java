72f96498c53fba856617ebf85cd7eabe481bf9c2
KyeongHoon Lee <leta6603@gmail.com>
2024-06-24 13:44:19 +0900

    Add '@FunctionalInterface' to RepeatCallback

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/repeat/RepeatCallback.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/repeat/RepeatCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2007 the original author or authors.
+ * Copyright 2006-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,8 +23,10 @@ package org.springframework.batch.repeat;
  *
  * @see RepeatOperations
  * @author Dave Syer
+ * @author KyeongHoon Lee
  *
  */
+@FunctionalInterface
 public interface RepeatCallback {
 
 	/**
