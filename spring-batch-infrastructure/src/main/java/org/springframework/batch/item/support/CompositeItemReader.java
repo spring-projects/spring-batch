403aeb6820c16de4b55d274448a81312f2fc65b5
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2024-10-03 09:36:27 +0200

    Add composite item reader implementation
    Resolves #757
    
    Signed-off-by: Fabrice Bibonne <fabrice.bibonne@gmail.com>

---
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/support/CompositeItemReader.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.item.support;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.ItemStreamException;
+import org.springframework.batch.item.ItemStreamReader;
+
+/**
+ * Composite reader that delegates reading to a list of {@link ItemStreamReader}s. This
+ * implementation is not thread-safe.
+ *
+ * @author Mahmoud Ben Hassine
+ * @param <T> type of objects to read
+ * @since 5.2
+ */
+public class CompositeItemReader<T> implements ItemStreamReader<T> {
+
+	private final List<ItemStreamReader<T>> delegates;
+
+	private final Iterator<ItemStreamReader<T>> delegatesIterator;
+
+	private ItemStreamReader<T> currentDelegate;
+
+	/**
+	 * Create a new {@link CompositeItemReader}.
+	 * @param delegates the delegate readers to read data
+	 */
+	public CompositeItemReader(List<ItemStreamReader<T>> delegates) {
+		this.delegates = delegates;
+		this.delegatesIterator = this.delegates.iterator();
+		this.currentDelegate = this.delegatesIterator.hasNext() ? this.delegatesIterator.next() : null;
+	}
+
+	// TODO: check if we need to open/close delegates on the fly in read() to avoid
+	// opening resources early for a long time
+	@Override
+	public void open(ExecutionContext executionContext) throws ItemStreamException {
+		for (ItemStreamReader<T> delegate : delegates) {
+			delegate.open(executionContext);
+		}
+	}
+
+	@Override
+	public T read() throws Exception {
+		if (this.currentDelegate == null) {
+			return null;
+		}
+		T item = currentDelegate.read();
+		if (item == null) {
+			currentDelegate = this.delegatesIterator.hasNext() ? this.delegatesIterator.next() : null;
+			return read();
+		}
+		return item;
+	}
+
+	@Override
+	public void update(ExecutionContext executionContext) throws ItemStreamException {
+		if (this.currentDelegate != null) {
+			this.currentDelegate.update(executionContext);
+		}
+	}
+
+	@Override
+	public void close() throws ItemStreamException {
+		for (ItemStreamReader<T> delegate : delegates) {
+			delegate.close();
+		}
+	}
+
+}
---
+++ spring-batch-samples/src/main/resources/org/springframework/batch/samples/compositereader/sql/schema.sql
@@ -0,0 +1,2 @@
+create table person_source (id int primary key, name varchar(20));
+create table person_target (id int primary key, name varchar(20));
