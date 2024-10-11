7c871e584c1cd888b3b459fb24518ffb419f5687
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2024-10-11 09:07:05 +0200

    Add blocking queue item reader and writer
    Resolves #2350
    Resolves #2044

---
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/queue/BlockingQueueItemReaderTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.springframework.batch.item.queue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.batch.item.queue.builder.BlockingQueueItemReaderBuilder;
+
+/**
+ * Test class for {@link BlockingQueueItemReader}.
+ *
+ * @author Mahmoud Ben Hassine
+ */
+class BlockingQueueItemReaderTests {
+
+	@Test
+	void testRead() throws Exception {
+		// given
+		BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
+		queue.put("foo");
+		BlockingQueueItemReader<String> reader = new BlockingQueueItemReaderBuilder<String>().queue(queue)
+			.timeout(10, TimeUnit.MILLISECONDS)
+			.build();
+
+		// when & then
+		Assertions.assertEquals("foo", reader.read());
+		Assertions.assertNull(reader.read());
+	}
+
+}
---
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/queue/builder/BlockingQueueItemWriterBuilderTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.springframework.batch.item.queue.builder;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.batch.item.queue.BlockingQueueItemReader;
+import org.springframework.batch.item.queue.BlockingQueueItemWriter;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Test class for {@link BlockingQueueItemWriterBuilder}.
+ *
+ * @author Mahmoud Ben Hassine
+ */
+class BlockingQueueItemWriterBuilderTests {
+
+	@Test
+	void testMandatoryQueue() {
+		assertThrows(IllegalStateException.class, () -> new BlockingQueueItemWriterBuilder<String>().build());
+	}
+
+	@Test
+	void testBuildWriter() {
+		// given
+		BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+
+		// when
+		BlockingQueueItemWriter<String> writer = new BlockingQueueItemWriterBuilder<String>().queue(queue).build();
+
+		// then
+		assertNotNull(writer);
+		assertEquals(queue, ReflectionTestUtils.getField(writer, "queue"));
+	}
+
+}
