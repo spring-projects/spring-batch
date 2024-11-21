c8c04d71edbc0c22e8440c471f2343ae99e85b71
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2024-11-21 02:18:34 +0100

    Optimize sequence handling in MongoDB job repository
    Signed-off-by: Fabrice Bibonne <fabrice.bibonne@gmail.com>

--- spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/MongoSequenceIncrementer.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/MongoSequenceIncrementer.java
@@ -15,20 +15,20 @@
  */
 package org.springframework.batch.core.repository.dao;
 
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.client.model.ReturnDocument;
+import org.bson.Document;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.jdbc.support.incrementer.DataFieldMaxValueIncrementer;
-
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
 
 // Based on https://www.mongodb.com/blog/post/generating-globally-unique-identifiers-for-use-with-mongodb
 // Section: Use a single counter document to generate unique identifiers one at a time
 
 /**
  * @author Mahmoud Ben Hassine
+ * @author Christoph Strobl
  * @since 5.2.0
  */
 public class MongoSequenceIncrementer implements DataFieldMaxValueIncrementer {
@@ -44,13 +44,11 @@ public class MongoSequenceIncrementer implements DataFieldMaxValueIncrementer {
 
 	@Override
 	public long nextLongValue() throws DataAccessException {
-		// TODO optimize
-		MongoSequence sequence = mongoTemplate.findOne(new Query(), MongoSequence.class, sequenceName);
-		Query query = query(where("_id").is(sequence.getId()));
-		Update update = new Update().inc("count", 1);
-		// The following does not return the modified document
-		mongoTemplate.findAndModify(query, update, MongoSequence.class, sequenceName);
-		return mongoTemplate.findOne(new Query(), MongoSequence.class, sequenceName).getCount();
+		return mongoTemplate.execute("BATCH_SEQUENCES",
+				collection -> collection
+					.findOneAndUpdate(new Document("_id", sequenceName), new Document("$inc", new Document("count", 1)),
+							new FindOneAndUpdateOptions().returnDocument(ReturnDocument.AFTER))
+					.getLong("count"));
 	}
 
 	@Override
@@ -61,35 +59,6 @@ public class MongoSequenceIncrementer implements DataFieldMaxValueIncrementer {
 	@Override
 	public String nextStringValue() throws DataAccessException {
 		throw new UnsupportedOperationException();
-	}
-
-	public static final class MongoSequence {
-
-		private String id;
-
-		private long count;
-
-		public String getId() {
-			return id;
-		}
-
-		public void setId(String id) {
-			this.id = id;
-		}
-
-		public long getCount() {
-			return count;
-		}
-
-		public void setCount(long count) {
-			this.count = count;
-		}
-
-		@Override
-		public String toString() {
-			return "MongoSequence{" + "id='" + id + '\'' + ", count=" + count + '}';
-		}
-
 	}
 
 }
