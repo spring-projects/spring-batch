cb62d3d14bafb55ce1865798c1d1cd7573334420
KyeongHoon Lee <leta6603@gmail.com>
2025-01-09 12:49:29 +0900

    remove repeating "used for" in comment
    Signed-off-by: KyeongHoon Lee <leta6603@gmail.com>

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/SqlPagingQueryUtils.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/SqlPagingQueryUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2024 the original author or authors.
+ * Copyright 2006-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,6 +32,7 @@ import org.springframework.util.StringUtils;
  * @author Michael Minella
  * @author Mahmoud Ben Hassine
  * @author Taeik Lim
+ * @author Kyeonghoon Lee
  * @since 2.0
  */
 public abstract class SqlPagingQueryUtils {
@@ -233,7 +234,7 @@ public abstract class SqlPagingQueryUtils {
 
 	/**
 	 * Generates ORDER BY attributes based on the sort keys.
-	 * @param provider the {@link AbstractSqlPagingQueryProvider} to be used for used for
+	 * @param provider the {@link AbstractSqlPagingQueryProvider} to be used for
 	 * pagination.
 	 * @return a String that can be appended to an ORDER BY clause.
 	 */
