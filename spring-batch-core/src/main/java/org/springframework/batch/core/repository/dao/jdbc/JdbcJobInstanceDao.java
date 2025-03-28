b88e7d70e9fe18f4c024aad010283600e97b8180
Yanming Zhou <zhouyanming@gmail.com>
2025-03-28 11:27:37 +0800

    Use `jdbcTemplate.queryForStream().findFirst()` where appropriate
    Signed-off-by: Yanming Zhou <zhouyanming@gmail.com>

--- spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/jdbc/JdbcJobInstanceDao.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/repository/dao/jdbc/JdbcJobInstanceDao.java
@@ -21,6 +21,7 @@ import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.springframework.batch.core.DefaultJobKeyGenerator;
 import org.springframework.batch.core.JobExecution;
@@ -173,21 +174,12 @@ public class JdbcJobInstanceDao extends AbstractJdbcBatchMetadataDao implements 
 
 		RowMapper<JobInstance> rowMapper = new JobInstanceRowMapper();
 
-		List<JobInstance> instances;
-		if (StringUtils.hasLength(jobKey)) {
-			instances = getJdbcTemplate().query(getQuery(FIND_JOBS_WITH_KEY), rowMapper, jobName, jobKey);
-		}
-		else {
-			instances = getJdbcTemplate().query(getQuery(FIND_JOBS_WITH_EMPTY_KEY), rowMapper, jobName, jobKey);
+		try (Stream<JobInstance> stream = getJdbcTemplate().queryForStream(
+				getQuery(StringUtils.hasLength(jobKey) ? FIND_JOBS_WITH_KEY : FIND_JOBS_WITH_EMPTY_KEY), rowMapper,
+				jobName, jobKey)) {
+			return stream.findFirst().orElse(null);
 		}
 
-		if (instances.isEmpty()) {
-			return null;
-		}
-		else {
-			Assert.state(instances.size() == 1, "instance count must be 1 but was " + instances.size());
-			return instances.get(0);
-		}
 	}
 
 	@Override
