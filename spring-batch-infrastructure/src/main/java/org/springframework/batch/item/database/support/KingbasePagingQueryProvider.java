59cf3310e862ad24aac006565fa0b6421e932b3f
zbw1231 <2313386675@qq.com>
2025-03-30 15:44:32 +0800

    add support for kingbase

---
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/item/database/support/KingbasePagingQueryProvider.java
@@ -0,0 +1,27 @@
+package org.springframework.batch.item.database.support;
+
+import org.springframework.util.StringUtils;
+
+
+public class KingbasePagingQueryProvider extends AbstractSqlPagingQueryProvider {
+
+    @Override
+    public String generateFirstPageQuery(int pageSize) {
+        return SqlPagingQueryUtils.generateLimitSqlQuery(this, false, buildLimitClause(pageSize));
+    }
+
+    @Override
+    public String generateRemainingPagesQuery(int pageSize) {
+        if (StringUtils.hasText(getGroupClause())) {
+            return SqlPagingQueryUtils.generateLimitGroupedSqlQuery(this, buildLimitClause(pageSize));
+        }
+        else {
+            return SqlPagingQueryUtils.generateLimitSqlQuery(this, true, buildLimitClause(pageSize));
+        }
+    }
+
+    private String buildLimitClause(int pageSize) {
+        return new StringBuilder().append("LIMIT ").append(pageSize).toString();
+    }
+
+}
