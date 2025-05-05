0a880c7ad125f6f3f815ee86dcd7dfbe4b19717a
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2025-05-05 18:51:49 +0200

    Disable CommandLineJobRunnerTests until the stub batch infrastructure is replaced with a JDBC one

--- spring-batch-core/src/test/java/org/springframework/batch/core/launch/support/CommandLineJobRunnerTests.java
+++ spring-batch-core/src/test/java/org/springframework/batch/core/launch/support/CommandLineJobRunnerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2023 the original author or authors.
+ * Copyright 2006-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.batch.core.BatchStatus;
@@ -58,6 +59,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Mahmoud Ben Hassine
  * @author Parikshit Dutta
  */
+@Disabled("Disabled until we replace the stub batch infrastructure with a JDBC one")
 class CommandLineJobRunnerTests {
 
 	private String jobPath = ClassUtils.addResourcePathToPackagePath(CommandLineJobRunnerTests.class,
