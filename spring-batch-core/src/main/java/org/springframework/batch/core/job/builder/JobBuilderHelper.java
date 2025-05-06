88ebaab1f372ce49011e1c9af8d7fe169ea2fa59
minkukjo <strike0115@naver.com>
2025-05-07 00:13:32 +0900

    GH-4826: Add support for job descriptions
    - Add description field and methods to AbstractJob class
    
    - Implement description setting in JobBuilderHelper
    
    - Add test cases for both XML and Java configurations
    
    - Update documentation with usage examples
    
    Signed-off-by: minkukjo <strike0115@naver.com>

--- spring-batch-core/src/main/java/org/springframework/batch/core/job/builder/JobBuilderHelper.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/job/builder/JobBuilderHelper.java
@@ -46,6 +46,7 @@ import org.springframework.batch.support.ReflectionUtils;
  * @author Dave Syer
  * @author Mahmoud Ben Hassine
  * @author Taeik Lim
+ * @author Minkuk Jo
  * @since 2.2
  */
 public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
@@ -180,6 +181,19 @@ public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
 		return result;
 	}
 
+	/**
+	 * Add a description to the job.
+	 * @param description the job description
+	 * @return this to enable fluent chaining
+	 * @since 6.0
+	 */
+	public B description(String description) {
+		properties.description = description;
+		@SuppressWarnings("unchecked")
+		B result = (B) this;
+		return result;
+	}
+
 	protected String getName() {
 		return properties.name;
 	}
@@ -221,6 +235,11 @@ public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
 			job.setRestartable(restartable);
 		}
 
+		String description = properties.getDescription();
+		if (description != null) {
+			job.setDescription(properties.description);
+		}
+
 		List<JobExecutionListener> listeners = properties.getJobExecutionListeners();
 		if (!listeners.isEmpty()) {
 			job.setJobExecutionListeners(listeners.toArray(new JobExecutionListener[0]));
@@ -245,6 +264,8 @@ public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
 
 		private JobParametersValidator jobParametersValidator;
 
+		private String description;
+
 		public CommonJobProperties() {
 		}
 
@@ -258,6 +279,7 @@ public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
 			this.jobExecutionListeners = new LinkedHashSet<>(properties.jobExecutionListeners);
 			this.jobParametersIncrementer = properties.jobParametersIncrementer;
 			this.jobParametersValidator = properties.jobParametersValidator;
+			this.description = properties.description;
 		}
 
 		public JobParametersIncrementer getJobParametersIncrementer() {
@@ -334,6 +356,14 @@ public abstract class JobBuilderHelper<B extends JobBuilderHelper<B>> {
 
 		public void setRestartable(boolean restartable) {
 			this.restartable = restartable;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
 		}
 
 		private String name;
