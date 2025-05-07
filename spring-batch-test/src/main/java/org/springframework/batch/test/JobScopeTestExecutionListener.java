6734282f14ebd5dfb7eb5359c4cfa11ab4dac1ab
coen <bht9011@gmail.com>
2025-05-08 08:16:41 +0900

    chore: deprecate junit4

--- spring-batch-test/src/main/java/org/springframework/batch/test/JobScopeTestExecutionListener.java
+++ spring-batch-test/src/main/java/org/springframework/batch/test/JobScopeTestExecutionListener.java
@@ -36,7 +36,7 @@ import org.springframework.util.ReflectionUtils.MethodCallback;
  * <pre>
  * &#064;ContextConfiguration
  * &#064;TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, JobScopeTestExecutionListener.class })
- * &#064;RunWith(SpringJUnit4ClassRunner.class)
+ * &#064;SpringJUnitConfig
  * public class JobScopeTestExecutionListenerIntegrationTests {
  *
  * 	// A job-scoped dependency configured in the ApplicationContext
@@ -62,6 +62,7 @@ import org.springframework.util.ReflectionUtils.MethodCallback;
  * @author Dave Syer
  * @author Jimmy Praet
  * @author Mahmoud Ben Hassine
+ * @author Hyuntae Park
  */
 public class JobScopeTestExecutionListener implements TestExecutionListener {
 
