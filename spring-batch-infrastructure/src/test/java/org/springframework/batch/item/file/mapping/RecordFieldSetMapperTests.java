43a323f91b2f6a95ca8c295adc4b38c1df49790b
isanghaessi <jesse1231@naver.com>
2025-07-15 00:12:04 +0900

    Fix RecordFieldSetMapper for empty record
    - add initialization for constructorParameterNames, constructorParameterTypes for empty record case.
    - add TC for empty record case.
    
    Signed-off-by: Seungyong Hong <jesse1231@naver.com>

--- spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/mapping/RecordFieldSetMapperTests.java
+++ spring-batch-infrastructure/src/test/java/org/springframework/batch/item/file/mapping/RecordFieldSetMapperTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2024 the original author or authors.
+ * Copyright 2020-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package org.springframework.batch.item.file.mapping;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.batch.item.file.transform.DefaultFieldSet;
 import org.springframework.batch.item.file.transform.FieldSet;
 
@@ -26,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * @author Mahmoud Ben Hassine
+ * @author Seungyong Hong
  */
 class RecordFieldSetMapperTests {
 
@@ -68,7 +68,23 @@ class RecordFieldSetMapperTests {
 		assertEquals("Field names must be specified", exception.getMessage());
 	}
 
+	@Test
+	void testMapFieldSetWhenEmptyRecord() {
+		// given
+		RecordFieldSetMapper<EmptyRecord> mapper = new RecordFieldSetMapper<>(EmptyRecord.class);
+		FieldSet fieldSet = new DefaultFieldSet(new String[0], new String[0]);
+
+		// when
+		EmptyRecord empty = mapper.mapFieldSet(fieldSet);
+
+		// then
+		assertNotNull(empty);
+	}
+
 	record Person(int id, String name) {
+	}
+
+	record EmptyRecord() {
 	}
 
 }
