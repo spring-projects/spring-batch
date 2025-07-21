20fbf28c0061df6c3524334ff04dbc13fc92aa40
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2025-07-21 07:58:16 +0200

    Redefine inherited non deprecated operational methods in TaskExecutorJobOperator

--- spring-batch-core/src/main/java/org/springframework/batch/core/launch/support/TaskExecutorJobOperator.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/launch/support/TaskExecutorJobOperator.java
@@ -16,8 +16,20 @@
 package org.springframework.batch.core.launch.support;
 
 import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.JobExecution;
+import org.springframework.batch.core.job.UnexpectedJobExecutionException;
+import org.springframework.batch.core.job.parameters.JobParameters;
+import org.springframework.batch.core.job.parameters.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
 import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.util.Assert;
 
 /**
  * A {@link org.springframework.core.task.TaskExecutor}-based implementation of the
@@ -41,13 +53,49 @@ import org.springframework.batch.core.repository.JobRepository;
 @SuppressWarnings("removal")
 public class TaskExecutorJobOperator extends SimpleJobOperator {
 
-	/**
-	 * Public setter for the {@link JobRegistry}.
-	 * @param jobRegistry the {@link JobRegistry} to set
-	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		super.afterPropertiesSet();
+	}
+
 	@Override
 	public void setJobRegistry(JobRegistry jobRegistry) {
+		Assert.notNull(jobRegistry, "JobRegistry must not be null");
 		this.jobRegistry = jobRegistry;
+	}
+
+	@Override
+	public JobExecution start(Job job, JobParameters jobParameters)
+			throws NoSuchJobException, JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException,
+			JobRestartException, JobParametersInvalidException {
+		Assert.notNull(job, "Job must not be null");
+		Assert.notNull(jobParameters, "JobParameters must not be null");
+		return super.start(job, jobParameters);
+	}
+
+	@Override
+	public JobExecution restart(JobExecution jobExecution) throws JobInstanceAlreadyCompleteException,
+			NoSuchJobExecutionException, NoSuchJobException, JobRestartException, JobParametersInvalidException {
+		Assert.notNull(jobExecution, "JobExecution must not be null");
+		return super.restart(jobExecution);
+	}
+
+	@Override
+	public JobExecution startNextInstance(Job job) throws UnexpectedJobExecutionException {
+		Assert.notNull(job, "Job must not be null");
+		return super.startNextInstance(job);
+	}
+
+	@Override
+	public boolean stop(JobExecution jobExecution) throws JobExecutionNotRunningException {
+		Assert.notNull(jobExecution, "JobExecution must not be null");
+		return super.stop(jobExecution);
+	}
+
+	@Override
+	public JobExecution abandon(JobExecution jobExecution) throws JobExecutionAlreadyRunningException {
+		Assert.notNull(jobExecution, "JobExecution must not be null");
+		return super.abandon(jobExecution);
 	}
 
 }
