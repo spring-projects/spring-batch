b9a96a64386139f433881ffffe1895a616bdcce0
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2026-01-21 18:00:26 +0100

    Update DDL migration script for IBM DB2 LUW
    Resolves #5166

--- spring-batch-core/src/main/resources/org/springframework/batch/core/migration/6.0/migration-db2.sql
+++ spring-batch-core/src/main/resources/org/springframework/batch/core/migration/6.0/migration-db2.sql
@@ -1,2 +1,5 @@
--- IBM DB2 LUW (10.1+)
+-- The following is only applicable to Informix servers.
+-- IBM DB2 LUW (10.1+) does not support sequence renaming: https://www.ibm.com/docs/en/db2/12.1.x?topic=statements-rename
+-- To address this limitation, use a stored procedure or a Java-based program that retrieves
+-- the last sequence number and creates a new sequence starting from that value.
 RENAME SEQUENCE BATCH_JOB_SEQ TO BATCH_JOB_INSTANCE_SEQ;
