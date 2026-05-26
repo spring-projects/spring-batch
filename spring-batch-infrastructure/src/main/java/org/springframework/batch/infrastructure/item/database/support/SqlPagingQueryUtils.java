d85cebd09a9ac07b592b676b2adb8b161afcfe7d
cladren123 <gold3987@naver.com>
2026-05-26 23:05:51 +0900

    Fix Comment in SqlPagingQueryUtils
    Signed-off-by: cladren123 <gold3987@naver.com>

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/database/support/SqlPagingQueryUtils.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/database/support/SqlPagingQueryUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2025 the original author or authors.
+ * Copyright 2006-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,7 @@ import org.springframework.util.StringUtils;
  * @author Michael Minella
  * @author Mahmoud Ben Hassine
  * @author Taeik Lim
+ * @author Dongeon Kim
  * @since 2.0
  */
 public abstract class SqlPagingQueryUtils {
@@ -176,7 +177,7 @@ public abstract class SqlPagingQueryUtils {
 
 	/**
 	 * Generates ORDER BY attributes based on the sort keys.
-	 * @param provider the {@link AbstractSqlPagingQueryProvider} to be used for used for
+	 * @param provider the {@link AbstractSqlPagingQueryProvider} to be used for
 	 * pagination.
 	 * @return a String that can be appended to an ORDER BY clause.
 	 */
