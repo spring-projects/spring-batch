526faed4261ce18299774edd4a55398ea148b2b4
Won Hyeonseob <dongjin778@naver.com>
2026-06-01 12:21:55 +0900

    GH-5185: Move async processing APIs to core
    Signed-off-by: Won Hyeonseob <dongjin778@naver.com>

---
+++ spring-batch-core/src/main/java/org/springframework/batch/core/step/item/AsyncItemProcessor.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2006-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.step.item;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import org.springframework.batch.core.step.StepExecution;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.core.listener.ItemProcessListener;
+import org.springframework.batch.core.scope.context.StepContext;
+import org.springframework.batch.core.scope.context.StepSynchronizationManager;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
+import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.util.Assert;
+
+/**
+ * An {@link ItemProcessor} that delegates to a nested processor and in the background. To
+ * allow for background processing the return value from the processor is a {@link Future}
+ * which needs to be unpacked before the item can be used by a client.
+ * <p>
+ * Because the {@link Future} is typically unwrapped in the {@link ItemWriter}, there are
+ * lifecycle and stats limitations (since the framework doesn't know what the result of
+ * the processor is). While not an exhaustive list, things like
+ * {@link StepExecution#getFilterCount()} will not reflect the number of filtered items
+ * and {@link ItemProcessListener#onProcessError(Object, Exception)} will not be called.
+ *
+ * @author Dave Syer
+ * @author Mahmoud Ben Hassine
+ * @param <I> the input object type
+ * @param <O> the output object type (will be wrapped in a Future)
+ * @see AsyncItemWriter
+ */
+public class AsyncItemProcessor<I, O> implements ItemProcessor<I, Future<O>> {
+
+	private ItemProcessor<I, O> delegate;
+
+	/**
+	 * Create a new {@link AsyncItemProcessor} with the delegate {@link ItemProcessor}.
+	 * @param delegate the {@link ItemProcessor} to use as a delegate
+	 * @since 6.0
+	 */
+	public AsyncItemProcessor(ItemProcessor<I, O> delegate) {
+		Assert.notNull(delegate, "The delegate ItemProcessor must not be null");
+		this.delegate = delegate;
+	}
+
+	private TaskExecutor taskExecutor = new SyncTaskExecutor();
+
+	/**
+	 * The {@link ItemProcessor} to use to delegate processing to in a background thread.
+	 * @param delegate the {@link ItemProcessor} to use as a delegate
+	 */
+	public void setDelegate(ItemProcessor<I, O> delegate) {
+		this.delegate = delegate;
+	}
+
+	/**
+	 * The {@link TaskExecutor} to use to allow the item processing to proceed in the
+	 * background. Defaults to a {@link SyncTaskExecutor} so no threads are created unless
+	 * this is overridden.
+	 * @param taskExecutor a {@link TaskExecutor}
+	 */
+	public void setTaskExecutor(TaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
+
+	/**
+	 * Transform the input by delegating to the provided item processor. The return value
+	 * is wrapped in a {@link Future} so that clients can unpack it later.
+	 *
+	 * @see ItemProcessor#process(Object)
+	 */
+	@SuppressWarnings("DataFlowIssue")
+	@Override
+	public @Nullable Future<O> process(I item) throws Exception {
+		final StepExecution stepExecution = getStepExecution();
+		FutureTask<O> task = new FutureTask<>(() -> {
+			if (stepExecution != null) {
+				StepSynchronizationManager.register(stepExecution);
+			}
+			try {
+				return delegate.process(item);
+			}
+			finally {
+				if (stepExecution != null) {
+					StepSynchronizationManager.close();
+				}
+			}
+		});
+		taskExecutor.execute(task);
+		return task;
+	}
+
+	/**
+	 * @return the current step execution if there is one
+	 */
+	private @Nullable StepExecution getStepExecution() {
+		StepContext context = StepSynchronizationManager.getContext();
+		if (context == null) {
+			return null;
+		}
+		return context.getStepExecution();
+	}
+
+}
--- spring-batch-integration/src/main/java/org/springframework/batch/integration/chunk/ChunkTaskExecutorItemWriter.java
+++ spring-batch-integration/src/main/java/org/springframework/batch/integration/chunk/ChunkTaskExecutorItemWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025-present the original author or authors.
+ * Copyright 2025-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,71 +15,19 @@
  */
 package org.springframework.batch.integration.chunk;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.scope.context.StepSynchronizationManager;
-import org.springframework.batch.core.step.StepContribution;
-import org.springframework.batch.core.step.StepExecution;
-import org.springframework.batch.core.listener.StepExecutionListener;
 import org.springframework.batch.core.step.item.ChunkProcessor;
-import org.springframework.batch.infrastructure.item.Chunk;
-import org.springframework.batch.infrastructure.item.ItemWriter;
 import org.springframework.core.task.TaskExecutor;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 /**
- * Similar to {@code ChunkMessageChannelItemWriter}, this item writer submits chunk
- * requests to local workers from a {@link TaskExecutor} instead of sending them over a
- * message channel to remote workers.
- *
- * <p>
- * The aggregation of worker contributions is done in the {@code afterStep} method, which
- * waits for all worker responses and updates the step execution accordingly. If any
- * worker response indicates a failure, the step execution is marked as failed and the
- * exception is added to the step execution's failure exceptions. Otherwise, the step
- * execution is marked as completed and the write counts and skip counts from all worker
- * contributions are aggregated into the step execution's write count and write skip
- * count. The commit count is also incremented for each successful worker contribution,
- * while the rollback count is incremented for each failed worker contribution.
- *
- * <p>
- * It should be noted that transaction management of the chunk as well as fault tolerance
- * features are not handled by this item writer and are the responsibility of the delegate
- * chunk processor.
- *
- * <p>
- * Moreover, the lifecycle of the task executor is not handled by this item writer.
- *
  * @param <T> type of items
- * @see ChunkMessageChannelItemWriter
  * @author Mahmoud Ben Hassine
  * @since 6.0
+ * @deprecated since 6.0.4 in favor of
+ * {@link org.springframework.batch.core.step.item.ChunkTaskExecutorItemWriter}
  */
-public class ChunkTaskExecutorItemWriter<T> implements ItemWriter<T>, StepExecutionListener {
-
-	private static final Log logger = LogFactory.getLog(ChunkTaskExecutorItemWriter.class);
-
-	@SuppressWarnings("NullAway.Init")
-	private StepExecution stepExecution;
-
-	private int sequence;
-
-	private final TaskExecutor taskExecutor;
-
-	private final ChunkProcessorChunkRequestHandler<T> chunkProcessorChunkHandler = new ChunkProcessorChunkRequestHandler<>();
-
-	private final Set<Future<ChunkResponse>> responses = new HashSet<>();
+@Deprecated(since = "6.0.4")
+public class ChunkTaskExecutorItemWriter<T>
+		extends org.springframework.batch.core.step.item.ChunkTaskExecutorItemWriter<T> {
 
 	/**
 	 * Create a new {@link ChunkTaskExecutorItemWriter}.
@@ -87,88 +35,7 @@ public class ChunkTaskExecutorItemWriter<T> implements ItemWriter<T>, StepExecut
 	 * @param taskExecutor the task executor to submit chunk processing tasks to
 	 */
 	public ChunkTaskExecutorItemWriter(ChunkProcessor<T> chunkRequestProcessor, TaskExecutor taskExecutor) {
-		this.taskExecutor = taskExecutor;
-		this.chunkProcessorChunkHandler.setChunkProcessor(chunkRequestProcessor);
-	}
-
-	@Override
-	public void write(Chunk<? extends T> chunk) {
-		ChunkRequest<T> request = new ChunkRequest<>(++sequence, chunk, this.stepExecution.getJobExecutionId(),
-				this.stepExecution.createStepContribution());
-		FutureTask<ChunkResponse> chunkResponseFutureTask = new FutureTask<>(() -> {
-			try {
-				StepSynchronizationManager.register(this.stepExecution);
-				return this.chunkProcessorChunkHandler.handle(request);
-			}
-			finally {
-				StepSynchronizationManager.close();
-			}
-		});
-		this.responses.add(chunkResponseFutureTask);
-		this.taskExecutor.execute(chunkResponseFutureTask);
-	}
-
-	@Override
-	public void beforeStep(StepExecution stepExecution) {
-		this.stepExecution = stepExecution;
-	}
-
-	@Override
-	public ExitStatus afterStep(StepExecution stepExecution) {
-		try {
-			ExitStatus result = ExitStatus.COMPLETED
-				.addExitDescription("Waited for " + this.responses.size() + " results.");
-			resetCounter(stepExecution);
-			for (StepContribution contribution : getStepContributions()) {
-				// only write counts and skip counts are aggregated here
-				// read counts and process/filter counts are managed by the driving step
-				stepExecution.setWriteCount(stepExecution.getWriteCount() + contribution.getWriteCount());
-				stepExecution.setWriteSkipCount(stepExecution.getWriteSkipCount() + contribution.getWriteSkipCount());
-				ExitStatus exitStatus = contribution.getExitStatus();
-				if (ExitStatus.FAILED.getExitCode().equals(exitStatus.getExitCode())) {
-					logger.error("Chunk processing failed for contribution: " + contribution
-							+ ", marking step execution as failed.");
-					result = exitStatus;
-					Throwable exitException = exitStatus.getExitException();
-					if (exitException != null) {
-						stepExecution.addFailureException(exitException);
-					}
-					stepExecution.setStatus(BatchStatus.FAILED);
-					stepExecution.incrementRollbackCount();
-				}
-				else {
-					stepExecution.incrementCommitCount();
-				}
-			}
-			return result;
-		}
-		catch (ExecutionException | InterruptedException e) {
-			stepExecution.setStatus(BatchStatus.FAILED);
-			stepExecution.addFailureException(e);
-			return ExitStatus.FAILED.addExitDescription(e);
-		}
-	}
-
-	/**
-	 * Reset the write count, write skip count, commit count and rollback count of the
-	 * step execution to avoid counting them twice as they are updated by workers and
-	 * aggregated in the afterStep method. The read count and process/filter count are not
-	 * reset as they are managed by the driving step
-	 * @param stepExecution the step execution to reset the counters for
-	 */
-	private void resetCounter(StepExecution stepExecution) {
-		stepExecution.setWriteCount(0);
-		stepExecution.setWriteSkipCount(0);
-		stepExecution.setCommitCount(0);
-		stepExecution.setRollbackCount(0);
-	}
-
-	private Collection<StepContribution> getStepContributions() throws ExecutionException, InterruptedException {
-		List<StepContribution> contributions = new ArrayList<>();
-		for (Future<ChunkResponse> task : this.responses) {
-			contributions.add(task.get().getStepContribution());
-		}
-		return contributions;
+		super(chunkRequestProcessor, taskExecutor);
 	}
 
 }
