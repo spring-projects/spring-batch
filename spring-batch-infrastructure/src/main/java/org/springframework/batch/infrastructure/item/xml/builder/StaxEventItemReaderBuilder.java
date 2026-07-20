72880cc89e03c2f8c4712864cec7bbacbd18b260
Gorock <daniel08ger@web.de>
2026-07-20 17:35:36 +0200

    Fix incorrect nullability contract for encoding in StaxEventItemReader
    The encoding can be null based on the documentation but Jspecify gives
    a warning when passing null, which is confusing.
    
    Signed-off-by: Daniel Morady <7807967+Gorock@users.noreply.github.com>

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/xml/builder/StaxEventItemReaderBuilder.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/xml/builder/StaxEventItemReaderBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2025 the original author or authors.
+ * Copyright 2017-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,7 @@ public class StaxEventItemReaderBuilder<T> {
 
 	private XMLInputFactory xmlInputFactory = StaxUtils.createDefensiveInputFactory();
 
-	private String encoding = StaxEventItemReader.DEFAULT_ENCODING;
+	private @Nullable String encoding = StaxEventItemReader.DEFAULT_ENCODING;
 
 	/**
 	 * Configure if the state of the {@link ItemStreamSupport} should be persisted within
@@ -202,7 +202,7 @@ public class StaxEventItemReaderBuilder<T> {
 	 * @return the current instance of the builder
 	 * @see StaxEventItemReader#setEncoding(String)
 	 */
-	public StaxEventItemReaderBuilder<T> encoding(String encoding) {
+	public StaxEventItemReaderBuilder<T> encoding(@Nullable String encoding) {
 		this.encoding = encoding;
 
 		return this;
