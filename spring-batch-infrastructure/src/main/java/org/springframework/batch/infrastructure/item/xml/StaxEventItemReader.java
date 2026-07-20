ff4e7cf9609db7dd83fb1335e5ce2af0206e5d6e
Gorock <daniel08ger@web.de>
2026-07-20 17:35:36 +0200

    Fix incorrect nullability contract for encoding in StaxEventItemReader
    The encoding can be null based on the documentation but Jspecify gives
    a warning when passing null, which is confusing.
    
    Signed-off-by: Daniel Morady <7807967+Gorock@users.noreply.github.com>

--- spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/xml/StaxEventItemReader.java
+++ spring-batch-infrastructure/src/main/java/org/springframework/batch/infrastructure/item/xml/StaxEventItemReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2025 the original author or authors.
+ * Copyright 2006-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -154,7 +154,7 @@ public class StaxEventItemReader<T> extends AbstractItemCountingItemStreamItemRe
 	 * @param encoding the encoding to be used. Can be {@code null}, in which case, the
 	 * XML event reader will attempt to auto-detect the encoding from tht input file.
 	 */
-	public void setEncoding(String encoding) {
+	public void setEncoding(@Nullable String encoding) {
 		this.encoding = encoding;
 	}
 
