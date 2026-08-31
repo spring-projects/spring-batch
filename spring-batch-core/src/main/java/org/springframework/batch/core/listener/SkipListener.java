8561ba1ee02540ae79f6a18679fff7d209c35419
Mahmoud Ben Hassine <mahmoud.ben-hassine@broadcom.com>
2026-08-31 21:51:36 +0200

    Clarify SkipListener transaction contract in Javadoc and reference docs
    Fixes gh-5494

--- spring-batch-core/src/main/java/org/springframework/batch/core/listener/SkipListener.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/listener/SkipListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2025 the original author or authors.
+ * Copyright 2006-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,18 @@ import org.springframework.batch.core.step.Step;
  * implementations at the appropriate time in the step lifecycle. Implementers of this
  * interface should not assume that any method is called immediately after an error has
  * been encountered. Because there may be errors later on in processing the chunk, this
- * listener is not called until just before committing.
+ * listener is not called until just before the outcome of the item, skip or step failure,
+ * is decided.
+ * <p>
+ * {@link #onSkipInRead} and {@link #onSkipInProcess} are called inside the chunk's
+ * ongoing transaction, before the rest of the chunk has been processed and written:
+ * whether that transaction ends up committed or rolled back depends on the outcome of the
+ * remaining items. {@link #onSkipInWrite} is called during a chunk scan, inside the
+ * single-item transaction used to re-attempt the faulty item, which <strong>is always
+ * rolled back</strong> right after this callback returns. <em>As a result, you should use
+ * {@code PROPAGATION_REQUIRES_NEW} for any transactional operation that is called from
+ * one of these methods</em>, consistent with {@link ChunkListener#onChunkError} and
+ * {@link ItemWriteListener#onWriteError}.
  *
  * @author Dave Syer
  * @author Robert Kasanicky
@@ -35,7 +46,8 @@ public interface SkipListener<T, S> extends StepListener {
 	 * Callback for a failure on read that is legal and, consequently, is not going to be
 	 * re-thrown. In case a transaction is rolled back and items are re-read, this
 	 * callback occurs repeatedly for the same cause. This happens only if read items are
-	 * not buffered.
+	 * not buffered. Called inside the chunk's ongoing transaction, which may or may not
+	 * be rolled back depending on the outcome of the rest of the chunk.
 	 * @param t cause of the failure
 	 */
 	default void onSkipInRead(Throwable t) {
@@ -43,6 +55,8 @@ public interface SkipListener<T, S> extends StepListener {
 
 	/**
 	 * This item failed on write with the given exception, and a skip was called for.
+	 * Called during a chunk scan, inside the single-item transaction used to re-attempt
+	 * this item, which is rolled back right after this callback returns.
 	 * @param item the failed item
 	 * @param t the cause of the failure
 	 */
@@ -51,6 +65,8 @@ public interface SkipListener<T, S> extends StepListener {
 
 	/**
 	 * This item failed on processing with the given exception, and a skip was called for.
+	 * Called inside the chunk's ongoing transaction, which may or may not be rolled back
+	 * depending on the outcome of the rest of the chunk.
 	 * @param item the failed item
 	 * @param t the cause of the failure
 	 */
